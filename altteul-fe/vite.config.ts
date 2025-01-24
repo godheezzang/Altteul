29adfe907bdbbf32dc56dc0641d2e8b3e186e184
박가희 <930_10@naver.com>
2025-01-24 12:11:27 +0900

    🎉 [FE] init: 프로젝트 세팅

---
+++ altteul-fe/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@router", replacement: "/src/router" },
+      { find: "@store", replacement: "/src/store" },
+      { find: "@utils", replacement: "/src/utils" },
+      { find: "@styles", replacement: "/src/styles" },
+    ],
+  },
+});
