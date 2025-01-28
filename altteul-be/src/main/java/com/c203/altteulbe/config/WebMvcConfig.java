4f33b569c688f90f6d484300a67ca42d4d8af5cb
jinwooseok <jinus7949@naver.com>
2025-01-29 03:25:34 +0900

    🔧 [BE] conf: swaggerConfig, WebMvcConfig 생성

---
+++ altteul-be/src/main/java/com/c203/altteulbe/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.c203.altteulbe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private static final long MAX_AGE_SECS = 3600;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:80")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.exposedHeaders("Authorization")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
+	}
+}
