2d1aef2fdea4e5232889b10400d63fbdb3bb8c3c
jinwooseok <jinus7949@naver.com>
2025-01-29 03:27:58 +0900

    ✨ [BE] feat: 커스텀 예외 처리 및 특정 예외 자동 처리

--- altteul-be/src/main/java/com/c203/altteulbe/AltteulBeApplication.java
+++ altteul-be/src/main/java/com/c203/altteulbe/AltteulBeApplication.java
@@ -2,8 +2,9 @@ package com.c203.altteulbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AltteulBeApplication {
 
 	public static void main(String[] args) {
