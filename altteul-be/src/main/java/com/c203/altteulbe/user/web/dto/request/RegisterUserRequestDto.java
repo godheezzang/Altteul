7082013e9d0f54a44b8a1fab5cada1a074e560c9
jinwooseok <jinus7949@naver.com>
2025-01-31 15:42:36 +0900

    ♻️ [BE] refactor: dto final 수정 및 쿼리문 수정

--- altteul-be/src/main/java/com/c203/altteulbe/user/web/dto/request/RegisterUserRequestDto.java
+++ altteul-be/src/main/java/com/c203/altteulbe/user/web/dto/request/RegisterUserRequestDto.java
@@ -8,22 +8,24 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class RegisterUserRequestDto implements AbstractRequestDto {
 
 	@NotNull(message = "Username is required")
-	private final String username;
+	private String username;
 
 	@NotNull(message = "Password is required")
 	@Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
-	private final String password;
+	private String password;
 
 	@NotNull(message = "Nickname is required")
-	private final String nickname;
+	private String nickname;
 
 	@NotNull(message = "Main language is required")
-	private final Language mainLang;
+	private Language mainLang;
 }
