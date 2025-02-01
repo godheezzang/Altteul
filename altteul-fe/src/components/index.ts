ae67c2c98a8323a7b27a5d5bf0c16f638ed88f21
김덕진 <ghaehfdl7@naver.com>
2025-02-01 20:08:14 +0900

    ✏️ [FE] typos: 폴더명 소문자로 변경

--- altteul-fe/src/components/index.ts
+++ altteul-fe/src/components/index.ts
@@ -1,5 +1,5 @@
 // 모달 관련 컴포넌트들을 한 번에 import
 
-export { default as Modal } from "./Modal/Modal";
+export { default as Modal } from "./modal/Modal";
 export { default as Button } from "./common/Button/Button";
 export { default as Input } from "./common/Input/Input";
