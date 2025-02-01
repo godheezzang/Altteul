956564178636f8a0062dd9e55b7dcf33338d9714
jinwooseok <jinus7949@naver.com>
2025-02-02 01:34:49 +0900

    ♻️ [BE] refactor: dto, entity 어노테이션 관리

--- altteul-be/src/main/java/com/c203/altteulbe/user/web/dto/request/LoginRequestDto.java
+++ altteul-be/src/main/java/com/c203/altteulbe/user/web/dto/request/LoginRequestDto.java
@@ -3,12 +3,9 @@ package com.c203.altteulbe.user.web.dto.request;
 import com.c203.altteulbe.common.dto.AbstractRequestDto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
