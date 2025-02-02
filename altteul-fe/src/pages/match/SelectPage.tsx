80019434a4f8c0220331489c27549c08592fb9d1
김덕진 <ghaehfdl7@naver.com>
2025-02-03 00:51:36 +0900

    ✏️ [FE] typos: Link 주소 소문자+하이픈 형식으로 변경

--- altteul-fe/src/pages/match/SelectPage.tsx
+++ altteul-fe/src/pages/match/SelectPage.tsx
@@ -24,10 +24,10 @@ const MatchingSelectPage = () => {
 
       {/* 컨텐츠 */}
       <div className="relative flex gap-x-[40vh] items-center">
-        {/* 개인전 링크 */}
+        {/* 개인전 버튼 링크 */}
         <div className="flex flex-col items-center">
           <Link 
-            to="/SingleSearch"
+            to="/single-search"
             className="w-[150px] h-[150px] rounded-full transition-all duration-500 hover:shadow-[0_0_30px_var(--primary-orange)]"
           >
             <img src={userIcon} alt="개인전" className="w-full h-full" />
@@ -35,10 +35,10 @@ const MatchingSelectPage = () => {
           <span className="mt-4 text-white text-[32px] font-bold">개인전</span>
         </div>
 
-        {/* 팀전 링크 */}
+        {/* 팀전 버튼 링크 */}
         <div className="flex flex-col items-center">
           <Link 
-            to="/TeamcompositionPage"
+            to="/team-composition"
             className="w-[150px] h-[150px] rounded-full transition-all duration-500 hover:shadow-[0_0_30px_var(--primary-orange)]"
           >
             <img src={peopleIcon} alt="팀전" className="w-full h-full" />
