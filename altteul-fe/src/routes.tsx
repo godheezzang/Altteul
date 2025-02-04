5c6a814f17bda9e87e00ac5040f39b05d1e96451
박가희 <930_10@naver.com>
2025-02-05 03:11:28 +0900

    ➕ [FE] add: '/' 경로 메인 페이지 추가

--- altteul-fe/src/routes.tsx
+++ altteul-fe/src/routes.tsx
@@ -1,3 +1,4 @@
+import Layout from "@components/Layout/Layout";
 import MainPage from "@pages/Main/MainPage";
 import SelectPage from "@pages/Match/SelectPage";
 import SingleFinalPage from "@pages/Match/SingleFinalPage";
@@ -5,58 +6,31 @@ import SingleSearchPage from "@pages/Match/SingleSearchPage";
 import TeamcompositionPage from "@pages/Match/TeamcompositionPage";
 import TeamFinalPage from "@pages/Match/TeamFinalPage";
 import TeamSearchPage from "@pages/Match/TeamSearchPage";
+import RankPage from "@pages/Rank/RankPage";
 import MyPage from "@pages/User/MyPage";
+import App from "App";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainPage />,
-  },
-  // 회원
-  {
-    path: "user",
+    element: <App />,
     children: [
       {
-        path: "mypage",
-        element: <MyPage />,
-      },
-    ],
-  },
-  {
-    path: "matching",
-    children: [
-      {
-        path: "select",
-        element: <SelectPage />,
+        index: true,
+        element: <MainPage />,
       },
       {
-        path: "team",
+        path: "rank",
+        element: <RankPage />,
+      },
+      // 회원
+      {
+        path: "user",
         children: [
           {
-            path: "composition",
-            element: <TeamcompositionPage />,
-          },
-          {
-            path: "search",
-            element: <TeamSearchPage />,
-          },
-          {
-            path: "final",
-            element: <TeamFinalPage />,
-          },
-        ],
-      },
-      {
-        path: "single",
-        children: [
-          {
-            path: "search",
-            element: <SingleSearchPage />,
-          },
-          {
-            path: "final",
-            element: <SingleFinalPage />,
+            path: "mypage",
+            element: <MyPage />,
           },
         ],
       },
@@ -66,7 +40,43 @@ const router = createBrowserRouter([
     path: "game",
     children: [
       {
-        path: "team",
+        path: "matching",
+        children: [
+          {
+            path: "select",
+            element: <SelectPage />,
+          },
+          {
+            path: "team",
+            children: [
+              {
+                path: "composition",
+                element: <TeamcompositionPage />,
+              },
+              {
+                path: "search",
+                element: <TeamSearchPage />,
+              },
+              {
+                path: "final",
+                element: <TeamFinalPage />,
+              },
+            ],
+          },
+          {
+            path: "single",
+            children: [
+              {
+                path: "search",
+                element: <SingleSearchPage />,
+              },
+              {
+                path: "final",
+                element: <SingleFinalPage />,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
