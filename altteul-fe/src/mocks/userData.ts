c5f8ae4103352c799cc2b4325293e97109b1a0d6
박가희 <930_10@naver.com>
2025-02-05 02:55:25 +0900

    ➕ [FE] add: 목업 데이터 추가

---
+++ altteul-fe/src/mocks/userData.ts
@@ -0,0 +1,62 @@
+import peopleIcon from "@assets/icon/people.svg";
+
+// TODO: api path 기록
+export const userData = [
+  {
+    userId: 1,
+    nickName: "알리언",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 2,
+    nickName: "샤샤샤",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 3,
+    nickName: "오리진",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 4,
+    nickName: "가희바희보",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 5,
+    nickName: "콜드캡슐",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 6,
+    nickName: "리카스",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+];
+
+export const TeamData = [
+  {
+    userId: 1,
+    nickName: "알리언",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 2,
+    nickName: "샤샤샤",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+  {
+    userId: 3,
+    nickName: "오리진",
+    profileImage: peopleIcon,
+    tierId: 3,
+  },
+];
