8cb2e13a28d7fa229de426a4f1d8c838c41a467e
박가희 <930_10@naver.com>
2025-02-05 02:58:41 +0900

    ♻️ [FE] refactor: 테스트용 코드 제거

--- altteul-fe/src/pages/Main/MainPage.tsx
+++ altteul-fe/src/pages/Main/MainPage.tsx
@@ -1,29 +1,7 @@
-import SignUpModal from "@components/Auth/SignUpModal";
-import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-
 const MainPage = () => {
-  // 회원가입 모달 PATH 사용 위한 로직 곧 삭제함
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    if (location.pathname === "/user/signup") {
-      setIsOpen(true);
-    } else {
-      setIsOpen(false);
-    }
-  }, [location]);
-
-  const closeModal = () => {
-    setIsOpen(false);
-    window.history.pushState(null, "", "/");
-  };
   return (
     <>
-      <h1>main</h1>
-      <SignUpModal isOpen={isOpen} onClose={closeModal} />
-      <Outlet />
+      <h1 className="text-primary-white">main</h1>
     </>
   );
 };
