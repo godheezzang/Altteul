0f007c0878e1d8a2ca2bcdbf1451499860504281
김성찬 <hjkim2661@gmail.com>
2025-02-04 11:35:29 +0900

    ♻️ [BE] refactor: 반환 형식 변경

--- altteul-be/src/main/java/com/c203/altteulbe/common/response/PageResponse.java
+++ altteul-be/src/main/java/com/c203/altteulbe/common/response/PageResponse.java
@@ -1,12 +1,8 @@
 package com.c203.altteulbe.common.response;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.data.domain.Page;
-
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,32 +12,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class PageResponse<T> {
-	private String key;
-	private List<T> content;
+	private List<T> key;
 	private int currentPage;
 	private int totalPages;
 	private long totalElements;
 	private boolean isLast;
 
 	public PageResponse(String key, Page<T> page) {
-		this.key = key;
-		this.content = page.getContent();
+		this.key = page.getContent();
 		this.currentPage = page.getNumber();
 		this.totalPages = page.getTotalPages();
 		this.totalElements = page.getTotalElements();
 		this.isLast = page.isLast();
 
-	}
-
-	@JsonAnyGetter
-	public Map<String, Object> getResponseData() {
-		Map<String, Object> responseData = new HashMap<>();
-		responseData.put(key, content);
-		responseData.put("currentPage", currentPage);
-		responseData.put("totalPages", totalPages);
-		responseData.put("totalElements", totalElements);
-		responseData.put("isLast", isLast);
-		return responseData;
 	}
 
 }
