74e6fc0b0ba3605a4e58f2fcf1c4851bb54eb240
박가희 <930_10@naver.com>
2025-02-05 17:17:29 +0900

    ➕ [FE] add: Ide 페이지 내 컴포넌트 생성 및 초기화

---
+++ altteul-fe/src/components/Ide/Editor.tsx
@@ -0,0 +1,11 @@
+const Editor = () => {
+  return (
+    <>
+      <div>
+        <h1>Editor</h1>
+      </div>
+    </>
+  );
+};
+
+export default Editor;
---
+++ altteul-fe/src/components/Ide/ProblemInfo.tsx
@@ -0,0 +1,11 @@
+const ProblemInfo = () => {
+  return (
+    <>
+      <div>
+        <h1>problem Info</h1>
+      </div>
+    </>
+  );
+};
+
+export default ProblemInfo;
