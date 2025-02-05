0d35250ef56d1eec3779b1e850151f30bf2e3eba
aodlfendb <heehyuon@gmail.com>
2025-02-05 17:42:40 +0900

    🚧 [FE] cont: 채팅's 공통 인풋 css 수정

--- altteul-fe/src/components/friend/FriendInput.tsx
+++ altteul-fe/src/components/friend/FriendInput.tsx
@@ -4,17 +4,31 @@ type FriendInputProps = {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
+  onSearch: () => void;
 };
 
-const FriendInput = ({ value, onChange, placeholder }: FriendInputProps) => {
+const FriendInput = ({
+  value,
+  onChange,
+  placeholder,
+  onSearch,
+}: FriendInputProps) => {
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      className="friend-input"
-    />
+    <div className="relative">
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="friend-input pr-10 py-2 pl-4 border border-orange-500 rounded-md w-full text-black"
+      />
+      <button
+        onClick={onSearch}
+        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-orange-500 text-xl"
+      >
+        버튼
+      </button>
+    </div>
   );
 };
 
