f382535ae238334689ac97950c0c72b366962d53
박가희 <930_10@naver.com>
2025-02-05 17:57:59 +0900

    ➕ [FE] add: 문제 정보 불러오는 로직 추가

--- altteul-fe/src/components/Ide/ProblemInfo.tsx
+++ altteul-fe/src/components/Ide/ProblemInfo.tsx
@@ -1,10 +1,38 @@
+import useGameStore from '@stores/useGameStore';
+
 const ProblemInfo = () => {
+  const problem = useGameStore((state) => state.problem);
+  const testcases = useGameStore((state) => state.testcases);
+
+  if (!problem || !testcases) {
+    return null;
+  }
+
   return (
-    <>
+    <div className='p-4 bg-gray-800'>
+      <h2 className='text-xl font-semibold mb-4'>
+        {problem.problemId}. {problem.problemTitle}
+      </h2>
       <div>
-        <h1>problem Info</h1>
+        <span className='text-gray-400'>문제 설명 </span>
+        <p className='mt-2'>{problem.description}</p>
       </div>
-    </>
+
+      <div className='mt-6'>
+        {testcases.map((testcase) => (
+          <div key={testcase.testcaseId} className='mb-4 p-3 rounded flex gap-2 '>
+            <div>
+              <p>입력 예시</p>
+              <div className='bg-gray-04 rounded p-3'>{testcase.input}</div>
+            </div>
+            <div>
+              <p>출력 예시</p>
+              <div className='bg-gray-04 rounded p-3'>{testcase.output}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
