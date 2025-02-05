d93b6e2f82011924b47efad4b29ad70809bc0948
박가희 <930_10@naver.com>
2025-02-05 17:55:26 +0900

    🪄 [FE] modify: gameState 속성 추가에 맞게 store 수정

--- altteul-fe/src/stores/useGameStore.ts
+++ altteul-fe/src/stores/useGameStore.ts
@@ -1,30 +1,18 @@
 // stores/useGameStore.ts
-import { mockGameData } from "mocks/gameData";
-import { Users, User } from "types/types";
-import { create } from "zustand";
-
-interface GameState {
-  problemId: number | null;
-  problemTitle: string | null;
-  users: Users;
-  setProblemInfo: (id: number, title: string) => void;
-  setUsers: (users: Users) => void;
-}
-
-// Userㄴ 배열 -> 객체로 변환
-const transformUsers = (users: User[]): Users =>
-  users.map((user) => ({
-    ...user,
-    nickName: user.nickName,
-    profileImg: user.profileImg,
-  }));
+import { GameState } from 'types/types';
+import { create } from 'zustand';
 
 const useGameStore = create<GameState>((set) => ({
-  problemId: mockGameData.data.problem.problemId,
-  problemTitle: mockGameData.data.problem.problemTitle,
-  users: transformUsers(mockGameData.data.users),
-  setProblemInfo: (id, title) => set({ problemId: id, problemTitle: title }),
+  gameId: null,
+  leaderId: null,
+  users: [],
+  problem: null,
+  testcases: [],
+
+  setGameInfo: (gameId, leaderId) => set({ gameId, leaderId }),
   setUsers: (users) => set({ users }),
+  setProblem: (problem) => set({ problem }),
+  setTestcases: (testcases) => set({ testcases }),
 }));
 
 export default useGameStore;
