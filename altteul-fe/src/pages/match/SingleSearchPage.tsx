bc576b9a17e359183004414c74c58f46b6daffa3
김덕진 <ghaehfdl7@naver.com>
2025-02-06 03:45:28 +0900

    🚧 [FE] cont: 유저 입장과 퇴장 그리고 게임 시작 기능 조치

--- altteul-fe/src/pages/match/SingleSearchPage.tsx
+++ altteul-fe/src/pages/match/SingleSearchPage.tsx
@@ -10,19 +10,55 @@ import logo from "@assets/icon/Altteul.svg";
 import tmi from "@assets/tmi.json";
 import { useTimer } from "@hooks/useTimer";
 import { User } from "types/types";
-import { mockSingleEnterData, mockSingleWatingData } from "mocks/singleData";
+import {
+  mockSingleEnterData,
+  mockUserInData,
+  mockUserOutData,
+} from "mocks/singleData";
 
+//페이지 렌더링 시 소켓 구독 요청필요
 const SingleSearchPage = () => {
   const navigate = useNavigate();
   const [fact, setFact] = useState<string>("");
   const [facts] = useState<string[]>(tmi.facts);
-  const [waitUsers, setWaitUsers] = useState(mockSingleEnterData.data.users)
+  const [waitUsers, setWaitUsers] = useState(mockSingleEnterData.data.users);
 
-  const userEnter = () => {
-    //유저 입장시 소켓 메세지의 users 부분 세팅
-    setWaitUsers(mockSingleWatingData.data.users)
+  //타이머 전 게임 시작 호출
+  const userStart = () => {
+    //8명 안됐는데 시작할거냐는 알림정도?(8명 되면 자동 시작)
+    if(confirm("8명 안됐는데 시작할거임?")) {
+      
+      //최소인원 확인
+      if(waitUsers.length >= 2) {
+        //넘어갈 때 현재 대기중인 유저(waitUsers) 정보 넘겨야함(소켓정보 유지 필요)
+        navigate("/match/single/final");
+      }else{
+        alert("개인전이긴 한데... 너 혼자 게임 못함...")
+      }
 
-  }
+    }
+
+  };
+
+  //새로운 유저 입장(소켓 메세지 핸들링 부분이 될듯)
+  const waitUserChange = (type: string) => {
+    if (type == "ENTER") {
+      //유저 입장시 소켓 메세지(파라미터랑 별개임)의 users 부분 세팅
+      setWaitUsers(mockUserInData.data.users);
+    }
+
+    if (type == "LEAVE") {
+      //남은 인원들로 waitUsers 재구성
+      setWaitUsers(mockUserOutData.data.remainingUsers);
+    }
+  };
+
+  //유저(본인) 퇴장
+  const userOut = () => {
+    // 구독 취소 요청 필요
+
+    navigate("/match/select");
+  };
 
   const { seconds } = useTimer({
     initialSeconds: 180, // 시작 시간 설정
@@ -46,14 +82,20 @@ const SingleSearchPage = () => {
     console.log(mockSingleEnterData.data);
     console.log(mockSingleEnterData.data.users);
   }, []);
-  
+
   return (
-    <div className="relative min-h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div
+      className="relative min-h-screen w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       {/* 배경 오버레이 */}
       <div className="absolute inset-0 bg-black/50"></div>
 
       {/* 로고 링크 */}
-      <Link to="/" className="z-20 absolute top-8 left-8 transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]">
+      <Link
+        to="/"
+        className="z-20 absolute top-8 left-8 transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]"
+      >
         <img src={logo} alt="홈으로" className="w-full h-full" />
       </Link>
 
@@ -63,20 +105,27 @@ const SingleSearchPage = () => {
         <div className="text-white text-4xl mb-8">{formatTime(seconds)}</div>
 
         {/* 방장: 리더아이디에 해당하는 유저 정보 넣어야 함*/}
-        <UserProfile nickName="방장" profileImage={peopleIcon} tierId={4} className="mb-4" />
+        <UserProfile
+          nickName="방장"
+          profileImage={peopleIcon}
+          tierId = {4}
+          className="mb-4"
+        />
 
         {/* 방장 이름 */}
         <div className="text-white text-2xl mb-4">나는 방장</div>
 
         {/* Status Message */}
         <div className="text-white text-xl mb-8 flex flex-col items-center">
-
-          {/* 임시로 텍스트 클릭시 새로운 유저 유입 만듬 */}
-          <div onClick={userEnter} >
+          {/* 임시로 텍스트 클릭시 새로운 유저 유입/유저 퇴장 부분(onClick) 만듬 */}
+          <div onClick={() => waitUserChange("ENTER")}>
             같이 플레이 할 상대를 찾고 있어요. 🧐
           </div>
 
-          <div className="flex text-base">
+          <div
+            className="flex text-base"
+            onClick={() => waitUserChange("LEAVE")}
+          >
             조금만 기다려 주세요
             <div className="ml-2">
               {/* 스피너 */}
@@ -87,25 +136,33 @@ const SingleSearchPage = () => {
 
         {/* 버튼 */}
         <div className="flex gap-6 mb-12">
-          <Link to="/match/single/final">
-            <Button width="160px" height="48px" className="transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]">
-              게임 시작
-            </Button>
-          </Link>
-          <Link to="/match/select">
-            <Button width="160px" height="48px" className="transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]">
-              매칭 취소하기
-            </Button>
-          </Link>
+          <Button
+            width="160px"
+            height="48px"
+            className="transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]"
+            onClick={() => userStart()}
+          >
+            게임 시작
+          </Button>
+          <Button
+            width="160px"
+            height="48px"
+            className="transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]"
+            onClick={() => userOut()}
+          >
+            매칭 취소하기
+          </Button>
         </div>
 
         {/* 방장 제외 대기 유저 */}
         <div className="flex justify-center items-center gap-20">
           {waitUsers.map((user: User) => (
-            <UserProfile key={user.userId} 
-                         nickName={user.nickname} 
-                         profileImage={user.profileImage}
-                         tierId= {user.tierId} />
+            <UserProfile
+              key={user.userId}
+              nickName={user.nickname}
+              profileImage={user.profileImage}
+              tierId={user.tierId}
+            />
           ))}
         </div>
 
