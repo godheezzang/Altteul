056aac6fa56a59a7452c7c85ba2f3bc6a9bb20d4
박가희 <930_10@naver.com>
2025-02-06 01:51:29 +0900

    💄 [FE] style: 개인전 IDE 페이지 그리드 스타일 추가

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -1,4 +1,7 @@
+import Editor from '@components/Ide/Editor';
+import GameUserList from '@components/Ide/GameUserList';
 import ProblemInfo from '@components/Ide/ProblemInfo';
+import Terminal from '@components/Ide/Terminal';
 import useGameStore from '@stores/useGameStore';
 import { mockGameData } from 'mocks/gameData';
 import { useEffect } from 'react';
@@ -13,11 +16,18 @@ const SingleIdePage = () => {
   }, []);
 
   return (
-    <div className='flex h-screen bg-gray-900'>
-      <div className='w-[300px]'>
+    <div className='grid grid-cols-16 h-screen bg-primary-black border-t border-gray-04'>
+      <div className='col-start-1 col-end-3 border-r border-gray-04'>
         <ProblemInfo />
       </div>
-      {/* 다른 컴포넌트들 */}
+      <div className='col-start-3 col-end-11 border-r border-gray-04'>
+        <Editor />
+        <Terminal />
+        <div>ide footer</div>
+      </div>
+      <div className='col-start-11 col-end-16'>
+        <GameUserList />
+      </div>
     </div>
   );
 };
