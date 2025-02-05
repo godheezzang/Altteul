f8f970d27fb94533a25ff43427f7535a02f4f383
김덕진 <ghaehfdl7@naver.com>
2025-02-06 02:41:35 +0900

    ♻️ [FE] refactor: 목업데이터 수정(새로운 유저 등장 부분)

--- altteul-fe/src/mocks/singleData.ts
+++ altteul-fe/src/mocks/singleData.ts
@@ -30,16 +30,26 @@ export const mockSingleWatingData = {   //대기중일 때 신규 상대 등장 
 		"roomId":"1",      // 입장한 방 id
 		"leaderId":"23",   // 방장 id
 		"users":[
-					{"userId":1,
-					 "nickname":"닉넴1",
-					 "profileImage":"",
-					 "tierId":1
-					 },
-					 {"userId":2,
-					 "nickname":"닉넴2",
-					 "profileImage":"",
-					 "tierId":1
-					 },
-                ]
-            },
+                    {"userId":1,
+                    "nickname":"오리진",
+                    "profileImage":"",
+                    "tierId":1
+                    },
+                    {"userId":2,
+                    "nickname":"리카스",
+                    "profileImage":"",
+                    "tierId":2
+                    },
+                    {"userId":3,
+                    "nickname":"콜드캐슬",
+                    "profileImage":"",
+                    "tierId":3
+                    },
+                    {"userId":4,
+                    "nickname":"newUser",
+                    "profileImage":"",
+                    "tierId":7
+                    },
+        ],
+    },
 }
