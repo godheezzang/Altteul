c353e2e158fea216b7f956919e2f4aa1e3226364
박가희 <930_10@naver.com>
2025-02-07 04:16:46 +0900

    ➕ [FE] add: 유저 페이지 관련 컴포넌트 생성 및 초기화

---
+++ altteul-fe/src/pages/User/UserPage.tsx
@@ -0,0 +1,15 @@
+import BattleRecord from '@components/user/BattleRecord';
+import UserInfo from '@components/user/UserInfo';
+
+const UserPage = () => {
+  return (
+    <div className='w-1/2 mx-auto'>
+      <div>
+        <UserInfo />
+        <BattleRecord />
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;
