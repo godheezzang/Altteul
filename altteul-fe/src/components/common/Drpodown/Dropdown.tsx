fffd0caec29502fd96786cb97fa9caccf7eed888
aodlfendb <heehyuon@gmail.com>
2025-02-06 10:05:13 +0900

    🚧 [FE] cont: 드롭다운 css 수정 중

--- altteul-fe/src/components/common/Drpodown/Dropdown.tsx
+++ altteul-fe/src/components/common/Drpodown/Dropdown.tsx
@@ -6,7 +6,6 @@ type DropdownProps = {
   onChange: (selected: string) => void;
   width?: string;
   height?: string;
-  className?: string;
 };
 
 const Dropdown = ({
@@ -15,18 +14,17 @@ const Dropdown = ({
   onChange,
   width,
   height,
-  className = "",
 }: DropdownProps) => {
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     onChange(e.target.value);
   };
 
   return (
     <select
-      className={`dropdown ${className}`.trim()}
       value={value}
       onChange={handleChange}
       style={{ width, height }}
+      className="text-gray-03 border border-gray-03 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-03"
     >
       {options.map((el) => (
         <option key={el.id} value={el.value}>
