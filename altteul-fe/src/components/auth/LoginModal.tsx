db94872c72a47b5ab9279c0eb29d040a84aef5d9
aodlfendb <heehyuon@gmail.com>
2025-02-06 20:18:07 +0900

    💄 [FE] style: 로그인 모달 디자인 수정

--- altteul-fe/src/components/auth/LoginModal.tsx
+++ altteul-fe/src/components/auth/LoginModal.tsx
@@ -6,7 +6,7 @@ import Button from "@components/Common/Button/Button";
 import axios from "axios";
 
 const LoginModal = ({ isOpen = false, onClose = () => {} }) => {
-  const [form, setForm] = useState({ id: "", password: "" });
+  const [form, setForm] = useState({ username: "", password: "" });
   const [error, setError] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +20,7 @@ const LoginModal = ({ isOpen = false, onClose = () => {} }) => {
     console.log("전송할 데이터:", form);
 
     try {
-      const response = await loginUser(form.id, form.password);
+      const response = await loginUser(form.username, form.password);
       console.log("로그인 성공:", response);
 
       if (!response) {
@@ -54,26 +54,58 @@ const LoginModal = ({ isOpen = false, onClose = () => {} }) => {
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} title="로그인">
-      <form onSubmit={handleSubmit}>
+    <Modal isOpen={isOpen} onClose={onClose} height="35rem" title="알뜰 로그인">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <Input
           type="text"
-          name="id"
-          placeholder="아이디를 입력해주세요"
-          value={form.id}
+          name="username"
+          placeholder="아이디를 입력해 주세요"
+          value={form.username}
+          className="mt-2"
           onChange={handleChange}
         />
         <Input
           type="password"
           name="password"
-          placeholder="비밀번호를 입력해주세요"
+          placeholder="비밀번호를 입력해 주세요"
           value={form.password}
           onChange={handleChange}
         />
         {error && <p>{error}</p>}
 
-        <Button type="submit" width="100%" height="50px">
+        <Button
+          type="submit"
+          width="100%"
+          height="2.8rem"
+          className="bg-primary-orange text-primary-white hover:brightness-90"
+        >
           로그인
+        </Button>
+
+        <Button
+          type="button"
+          width="100%"
+          height="2.8rem"
+          className="bg-gray-01 hover:brightness-90"
+        >
+          회원가입
+        </Button>
+
+        <a
+          href="#"
+          className="text-right text-gray-03 cursor-pointer underline hover:font-semibold"
+        >
+          비밀번호 재설정
+        </a>
+
+        <Button
+          type="button"
+          width="100%"
+          height="2.8rem"
+          className="bg-primary-black text-primary-white mt-8 hover:brightness-110"
+          img="src/assets/icon/github_logo.svg"
+        >
+          github로 간편하게 시작하기
         </Button>
       </form>
     </Modal>
