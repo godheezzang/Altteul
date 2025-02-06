76ecab92eeef1c23db80366c92896b3c15242256
박가희 <930_10@naver.com>
2025-02-06 10:23:05 +0900

    💄 [FE] style: 싱글페이지 내 세 컴포넌트 정렬 grid -> flex로 변경

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -16,16 +16,16 @@ const SingleIdePage = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-16 h-screen bg-primary-black border-t border-gray-04'>
-      <div className='col-start-1 col-end-3 border-r border-gray-04'>
+    <div className='flex h-screen bg-primary-black border-t border-gray-04'>
+      <div className=' min-w-[30rem] border-r border-gray-04'>
         <ProblemInfo />
       </div>
-      <div className='col-start-3 col-end-11 border-r border-gray-04'>
+      <div className='max-w-[65rem] flex-[46rem] border-r border-gray-04'>
         <CodeEditor />
         <Terminal />
         <div>ide footer</div>
       </div>
-      <div className='col-start-11 col-end-16'>
+      <div className='min-w-[20rem]'>
         <GameUserList />
       </div>
     </div>
