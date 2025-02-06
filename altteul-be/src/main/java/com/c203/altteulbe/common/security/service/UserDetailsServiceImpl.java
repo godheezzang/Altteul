e99ce7e8d2cca19bafc08e85194be2048798a8f2
jinwooseok <jinus7949@naver.com>
2025-02-06 10:23:07 +0900

    🚧 [BE] cont: 로그인 username으로 수정

--- altteul-be/src/main/java/com/c203/altteulbe/common/security/service/UserDetailsServiceImpl.java
+++ altteul-be/src/main/java/com/c203/altteulbe/common/security/service/UserDetailsServiceImpl.java
@@ -24,8 +24,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepository.findByUsername(username)
+		return userRepository.findByUsername(username)
 			.orElseThrow(NotFoundUserException::new);
-		return user;
 	}
 }
