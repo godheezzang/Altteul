90192f64249bef9c9abd75a242d1e4cf949e78ad
박가희 <930_10@naver.com>
2025-02-07 13:59:01 +0900

    🚧 [FE] cont: 유저 게임 기록 개발 중

--- altteul-fe/src/components/user/BattleRecord.tsx
+++ altteul-fe/src/components/user/BattleRecord.tsx
@@ -1,4 +1,27 @@
+import { getUserRecord } from '@utils/api/userApi';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { UserGameRecord } from 'types/types';
+
 const BattleRecord = () => {
+  const userId = useParams();
+  const [Record, setRecord] = useState<UserGameRecord | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserRecord = async () => {
+      try {
+        setIsLoading(true);
+        let data;
+
+        if (userId) {
+          const response = await getUserRecord(userId);
+        } else {
+          return;
+        }
+      } catch (error) {}
+    };
+  });
   return (
     <>
       <h1>BattleRecord</h1>
