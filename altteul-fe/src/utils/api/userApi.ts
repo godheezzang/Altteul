d0035bb712b7dde1e7baf78767c491fe1a461139
박가희 <930_10@naver.com>
2025-02-08 19:52:43 +0900

    ✨ [FE] feat: 유저 게임 정보 불러오는 로직 추가

--- altteul-fe/src/utils/api/userApi.ts
+++ altteul-fe/src/utils/api/userApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { gameRecordData } from 'mocks/gameRecordData';
 import { UserGameRecordResponse, UserInfoResponse } from 'types/types';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
@@ -16,15 +17,23 @@ export const getUserInfo = async (userId: string, token: string = '') => {
   return response.data;
 };
 
-export const getUserRecord = async (userId: string, lang: string, type: string) => {
-  const response = await axios.get<UserGameRecordResponse>(`${BASE_URL}/api/game/history/${userId}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    params: {
-      lang,
-      type,
-    },
+export const getUserRecord = async (userId: string, lang: string, type: string): Promise<UserGameRecordResponse> => {
+  // API 개발되면 주석 해제
+  // const response = await axios.get<UserGameRecordResponse>(`${BASE_URL}/api/game/history/${userId}`, {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   params: {
+  //     lang,
+  //     type,
+  //   },
+  // });
+  // return response.data;
+
+  // 목 데이터 사용
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(gameRecordData);
+    }, 500); // 실제 API 호출을 시뮬레이션하기 위해 조금 기다렸다가 띄움
   });
-  return response.data;
 };
