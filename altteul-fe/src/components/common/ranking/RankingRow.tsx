6b24a1bc5d68d4311ee45f24e4e468b5475a4bbc
김덕진 <ghaehfdl7@naver.com>
2025-02-08 21:04:58 +0900

    ✨ [FE] feat: 티어에 따른 뱃지 이미지 표기 수정

--- altteul-fe/src/components/common/ranking/RankingRow.tsx
+++ altteul-fe/src/components/common/ranking/RankingRow.tsx
@@ -1,14 +1,12 @@
-import badge from "@assets/icon/Badge_09.svg";
 import rank_up from "@assets/icon/rank_up.svg";
 import rank_down from "@assets/icon/rank_down.svg";
-
+import {RankingResponse} from "types/types"
 // 랭킹 행 컴포넌트
-const RankingRow = ({ data }) => {
-  const { userId, nickname, mainLang, rank, rankPoint, tierId, rankChange } =
-    data;
+const RankingRow = ({ data }: { data: RankingResponse }) => {
+  const { nickname, mainLang, rank, rankPoint, tierId, rankChange } = data;
 
   const formatNumber = (num: number) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num ? num.toLocaleString() : '0';
   };
 
   const getLanguageDisplay = (lang: string) => {
@@ -34,14 +32,32 @@ const RankingRow = ({ data }) => {
     );
   };
 
+  const getBadgeImage = () => {
+    const badge =
+      rank == 1
+        ? "/src/assets/icon/Badge/Badge_09.svg"
+        : tierId == 1
+        ? "/src/assets/icon/Badge/Badge_01.svg"
+        : tierId == 4
+        ? "/src/assets/icon/Badge/Badge_04.svg"
+        : tierId == 5
+        ? "/src/assets/icon/Badge/Badge_05.svg"
+        : tierId == 7
+        ? "/src/assets/icon/Badge/Badge_07.svg"
+        : tierId == 8
+        ? "/src/assets/icon/Badge/Badge_08.svg"
+        : "";
+    return <img src={badge} alt="뱃지" className="w-6 h-6" />;
+  };
+
   return (
     // 한 행
-    <div className="grid grid-cols-5 py-4 px-6 bg-primary-black/30 text-primary-white text-center">
+    <div className="grid grid-cols-5 py-4 px-6 bg-primary-black/30 text-primary-white text-center py-6">
       {/* 순위 */}
       <div className="grid justify-items-start ml-5 text-center">{rank}</div>
       {/* 뱃지&닉네임 */}
       <div className="flex">
-        <img src={badge} alt="뱃지" className="w-6 h-6" />
+        <div>{getBadgeImage()}</div>
         <div>{nickname}</div>
       </div>
       {/* 순위변동 */}
