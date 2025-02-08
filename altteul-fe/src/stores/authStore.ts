10a62a65fc44a3c2af2d6bbafdbcd3f038c922f7
박가희 <930_10@naver.com>
2025-02-08 17:22:05 +0900

    ➕ [FE] add: AuthState 인터페이스 추가

--- altteul-fe/src/stores/authStore.ts
+++ altteul-fe/src/stores/authStore.ts
@@ -1,15 +1,21 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+
+interface AuthState {
+  token: string;
+  setToken: (newToken: string) => void;
+  logout: () => void;
+}
 
 const useAuthStore = create<AuthState>((set) => ({
-  token: localStorage.getItem("token") || "",
+  token: localStorage.getItem('token') || '',
   setToken: (newToken: string) => {
-    const cleanToken = newToken.replace(/^Bearer\s+/i, "");
-    localStorage.setItem("token", cleanToken); //로컬에 저장
+    const cleanToken = newToken.replace(/^Bearer\s+/i, '');
+    localStorage.setItem('token', cleanToken); //로컬에 저장
     set({ token: cleanToken }); // zustand에 저장
   },
   logout: () => {
-    localStorage.removeItem("token");
-    set({ token: "" });
+    localStorage.removeItem('token');
+    set({ token: '' });
   },
 }));
 
