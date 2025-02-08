af24cd1e5073b9bcf31d4ed4c27a653db828594e
aodlfendb <heehyuon@gmail.com>
2025-02-09 03:30:07 +0900

    ♻️ [FE] refactor: 인풋 스타일 코드 개선

--- altteul-fe/src/components/common/Input.tsx
+++ altteul-fe/src/components/common/Input.tsx
@@ -1,10 +1,8 @@
-// focus했을 때 주황 테두리 되도록 해야함
-
 import React from "react";
 
 type InputProps = {
-  type: "text" | "password" | "search";
-  placeholder: string;
+  type?: "text" | "password" | "search";
+  placeholder?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   name: string;
@@ -13,9 +11,12 @@ type InputProps = {
   className?: string;
 };
 
+const DEFAULT_INPUT_STYLE =
+  "text-lg font-light text-primary-black bg-primary-white border-2 border-gray-02 rounded-lg px-4 py-2 w-full focus:ring-3 focus:ring-primary-orange focus:outline-none";
+
 const Input = ({
   type = "text",
-  placeholder,
+  placeholder = "내용을 입력하세요",
   value,
   onChange,
   name,
@@ -25,13 +26,12 @@ const Input = ({
 }: InputProps) => {
   return (
     <input
-      className={`text-lg font-light text-primary-black bg-primary-white border-2 border-gray-02 rounded-lg px-4 py-2 w-full focus:ring-3 focus:ring-primary-orange focus:outline-none ${className}`.trim()}
+      className={`${DEFAULT_INPUT_STYLE} w-[${width}] h-[${height}] ${className}`}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
       name={name}
-      style={{ width, height }}
     />
   );
 };
