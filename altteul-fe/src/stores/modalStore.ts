bf39e1ff9a8fa6f2c453c3356561744e81ad19c2
김덕진 <ghaehfdl7@naver.com>
2025-02-09 22:16:47 +0900

    ✨ [FE] feat: 모달 정보를 담기 위한 modalInfo속성 추가

--- altteul-fe/src/stores/modalStore.ts
+++ altteul-fe/src/stores/modalStore.ts
@@ -1,17 +1,29 @@
+// src/stores/modalStore.ts
 import { create } from "zustand";
+import { ModalInfo } from "types/modalTypes";
 
 interface ModalState {
   currentModal: string | null;
-  openModal: (modalName: string) => void;
+  modalInfo: ModalInfo | null;
+  openModal: (modalName: string, info?: ModalInfo) => void;
   closeModal: () => void;
   isOpen: (modalName: string) => boolean;
+  getModalInfo: () => ModalInfo | null;
 }
 
 const useModalStore = create<ModalState>((set, get) => ({
   currentModal: null,
-  openModal: (modalName) => set({ currentModal: modalName }),
-  closeModal: () => set({ currentModal: null }),
+  modalInfo: null,
+  openModal: (modalName, info = null) => set({ 
+    currentModal: modalName,
+    modalInfo: info
+  }),
+  closeModal: () => set({ 
+    currentModal: null,
+    modalInfo: null 
+  }),
   isOpen: (modalName) => get().currentModal === modalName,
+  getModalInfo: () => get().modalInfo,
 }));
 
 export default useModalStore;
