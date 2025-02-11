ce8111a33d176d9a5918e2b66ec0c5907b4c89f2
aodlfendb <heehyuon@gmail.com>
2025-02-11 22:29:32 +0900

    ✅ [FE] update: 날아간 import 주워오기, 사진 import로 가져오는걸로 바꿈

--- altteul-fe/src/components/friend/friend_common/Navigation.tsx
+++ altteul-fe/src/components/friend/friend_common/Navigation.tsx
@@ -1,49 +1,41 @@
 // components/friend/Navigation.tsx
+import Friend_list from '@assets/icon/friend/Friend_list.svg';
+import Chat_bubble from '@assets/icon/friend/Chat_bubble.svg';
+import Notifications from '@assets/icon/friend/Notifications.svg';
+
 type NavigationProps = {
-  currentTab: "friends" | "chat" | "notifications";
-  onNavigate: (tab: "friends" | "chat" | "notifications") => void;
+  currentTab: 'friends' | 'chat' | 'notifications';
+  onNavigate: (tab: 'friends' | 'chat' | 'notifications') => void;
 };
 
 const Navigation = ({ currentTab, onNavigate }: NavigationProps) => {
   return (
     <div className="mt-4 flex justify-around border-t border-orange-300 pt-3">
       <button
-        onClick={() => onNavigate("friends")}
+        onClick={() => onNavigate('friends')}
         className={`relative flex flex-col items-center p-1 hover:scale-110 rounded-lg ${
-          currentTab === "friends" ? "text-primary-orange" : "text-gray-02"
+          currentTab === 'friends' ? 'text-primary-orange' : 'text-gray-02'
         }`}
       >
-        <img
-          src="/src/assets/icon/Friend_list.svg"
-          alt="친구목록"
-          className="w-10 h-10"
-        />
+        <img src={Friend_list} alt="친구목록" className="w-10 h-10" />
       </button>
 
       <button
-        onClick={() => onNavigate("chat")}
+        onClick={() => onNavigate('chat')}
         className={`relative flex flex-col items-center px-4 py-2 hover:scale-110 rounded-lg ${
-          currentTab === "chat" ? "text-primary-orange" : "text-gray-02"
+          currentTab === 'chat' ? 'text-primary-orange' : 'text-gray-02'
         }`}
       >
-        <img
-          src="/src/assets/icon/Chat_bubble.svg"
-          alt="채팅목록"
-          className="w-9 h-9"
-        />
+        <img src={Chat_bubble} alt="채팅목록" className="w-9 h-9" />
       </button>
 
       <button
-        onClick={() => onNavigate("notifications")}
+        onClick={() => onNavigate('notifications')}
         className={`relative flex flex-col items-center px-4 py-2 hover:scale-110 rounded-lg ${
-          currentTab === "notifications" ? "text-primary-orange" : "text-gray-02"
+          currentTab === 'notifications' ? 'text-primary-orange' : 'text-gray-02'
         }`}
       >
-        <img
-          src="/src/assets/icon/Notifications.svg"
-          alt="알림목록"
-          className="w-9 h-9"
-        />
+        <img src={Notifications} alt="알림목록" className="w-9 h-9" />
       </button>
     </div>
   );
