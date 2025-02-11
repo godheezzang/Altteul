c1456bb61b8548615c48f23ad7759621a80c1519
박가희 <930_10@naver.com>
2025-02-12 05:01:23 +0900

    ♻️ [FE] refactor: props 타입 명확히 지정

--- altteul-fe/src/components/Ide/IdeFooter.tsx
+++ altteul-fe/src/components/Ide/IdeFooter.tsx
@@ -1,19 +1,18 @@
 import SmallButton from '@components/Common/Button/SmallButton ';
-import useCodeExecutionStore from '@stores/useCodeExecutionStore';
 
 interface IdeFooterProps {
   onExecute: () => void;
 }
 
-const IdeFooter: React.FC<IdeFooterProps> = ({ onExecute }) => {
+const IdeFooter = ({ onExecute }: IdeFooterProps) => {
   const handleSubmitCode = () => {
     // 코드 제출 axios
   };
 
   return (
-    <div className='flex justify-end items-center p-2 bg-primary-black border-t border-gray-04'>
-      <SmallButton onClick={onExecute} children='코드 실행' backgroundColor='gray-04'/>
-      <SmallButton onClick={handleSubmitCode} children='코드 제출' type='submit' />
+    <div className="flex justify-end items-center p-2 bg-primary-black border-t border-gray-04">
+      <SmallButton onClick={onExecute} children="코드 실행" backgroundColor="gray-04" />
+      <SmallButton onClick={handleSubmitCode} children="코드 제출" type="submit" />
     </div>
   );
 };
