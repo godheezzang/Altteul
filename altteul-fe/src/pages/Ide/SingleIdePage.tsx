cb67b93ba74258cf5e2619795f975b360f6119d4
박가희 <930_10@naver.com>
2025-02-12 05:03:53 +0900

    🚧 [FE] cont: 개인전 Ide 수정중

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -1,19 +1,67 @@
-import { IdeLayout } from '@components/Ide/IdeLayout';
-import { useIde } from '@hooks/useIde';
+import { useEffect, useState } from 'react';
+import useGameWebSocket from '@hooks/useGameWebSocket';
+import useGameStore from '@stores/useGameStore';
+import CodeEditor from '@components/Ide/CodeEditor';
+import Terminal from '@components/Ide/Terminal';
+import IdeFooter from '@components/Ide/IdeFooter';
+import ProblemInfo from '@components/Ide/ProblemInfo';
+import SideProblemModal from '@components/Ide/SideProblemModal';
 
-// 게임 시작 후 소켓 연결
-// 사이드 문제 출제, 제출
-// 코드 실행: api
-// 코드 제출: socket
-// 코드 정답 맞춤 -> 합격 여부, 맞은 개수, 전체 tc 개수, 실행 시간, 메모리
-// 실패 -> 그대로 다시 문제 풀기
-
-// 코드 결과창
-// AI 코칭 외 다른 버튼 클릭 시 구독 끊고 페이지 이동
-
-// TODO: 팀전과 개인전 페이지 분리 필요
 const SingleIdePage = () => {
-  return <IdeLayout {...useIde(false)} />;
+  const { gameId, roomId, users, problem } = useGameStore();
+  const {
+    sideProblem,
+    sideProblemResult,
+    codeResult,
+    opponentCodeResult,
+    requestSideProblem,
+    submitSideProblemAnswer,
+    submitCode,
+  } = useGameWebSocket(gameId, roomId);
+
+  const [code, setCode] = useState('');
+  const [language, setLanguage] = useState<'python' | 'java'>('python');
+  const [showModal, setShowModal] = useState(false);
+
+  // ✅ 5분마다 자동으로 사이드 문제 요청
+  useEffect(() => {
+    const interval = setInterval(() => {
+      requestSideProblem();
+    }, 10 * 1000); // 5분
+
+    return () => clearInterval(interval);
+  }, [requestSideProblem]);
+
+  // ✅ 사이드 문제가 도착하면 모달 띄우기
+  useEffect(() => {
+    if (sideProblem) {
+      setShowModal(true);
+    }
+  }, [sideProblem]);
+
+  return (
+    <div className="flex h-screen bg-primary-black border-t border-gray-04">
+      <div className="min-w-[30rem] border-r border-gray-04">
+        <ProblemInfo />
+      </div>
+
+      <div className="max-w-[65rem] flex-[46rem] border-r border-gray-04">
+        <CodeEditor code={code} setCode={setCode} language={language} setLanguage={setLanguage} />
+        <Terminal output={codeResult ? JSON.stringify(codeResult, null, 2) : ''} />
+        <IdeFooter onExecute={() => submitCode(problem.problemId, language, code)} />
+      </div>
+      {/* ✅ 사이드 문제 모달 */}
+      {showModal &&
+        sideProblem(
+          <SideProblemModal
+            gameId={gameId}
+            roomId={roomId}
+            problem={sideProblem?.data}
+            onClose={() => setShowModal(false)}
+          />
+        )}
+    </div>
+  );
 };
 
 export default SingleIdePage;
