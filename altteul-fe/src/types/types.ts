4d0574533469b81ae86a5f56da654d04e95176a9
박가희 <930_10@naver.com>
2025-02-11 11:54:13 +0900

    ✅ [FE] update: gameId 저장 로직 추가

--- altteul-fe/src/types/types.ts
+++ altteul-fe/src/types/types.ts
@@ -1,140 +1,141 @@
 export interface User {
-	userId: number;
-	nickname: string;
-	profileImg: string;
-	tierId: number;
+  userId: number;
+  nickname: string;
+  profileImg: string;
+  tierId: number;
 }
 
 export type Users = User[];
 
 export interface TestCase {
-	testcaseId: number;
-	number: number;
-	input: string;
-	output: string;
+  testcaseId: number;
+  number: number;
+  input: string;
+  output: string;
 }
 
 export interface Problem {
-	problemId: number;
-	problemTitle: string;
-	description: string;
+  problemId: number;
+  problemTitle: string;
+  description: string;
 }
 
 export interface GameState {
-	gameId: number | null;
-	leaderId: number | null;
-	users: User[];
-	problem: Problem | null;
-	testcases: TestCase[];
-	setGameInfo: (gameId: number, leaderId: number) => void;
-	setUsers: (users: User[]) => void;
-	setProblem: (problem: Problem) => void;
-	setTestcases: (testcases: TestCase[]) => void;
+  gameId: number | null;
+  leaderId: number | null;
+  users: User[];
+  problem: Problem | null;
+  testcases: TestCase[];
+  setGameInfo: (gameId: number, leaderId: number) => void;
+  setUsers: (users: User[]) => void;
+  setProblem: (problem: Problem) => void;
+  setTestcases: (testcases: TestCase[]) => void;
 }
 
-type Language = "python" | "java";
+type Language = 'python' | 'java';
 
 export interface CodeExecutionState {
-	code: string;
-	language: Language;
-	output: string[];
-	setCode: (code: string) => void;
-	setLanguage: (language: Language) => void;
-	executeCode: () => void;
-	clearOutput: () => void;
+  code: string;
+  language: Language;
+  output: string[];
+  setCode: (code: string) => void;
+  setLanguage: (language: Language) => void;
+  executeCode: () => void;
+  clearOutput: () => void;
 }
 
 export interface UserInfoResponse {
-	status: number;
-	message: string;
-	data: UserInfo;
+  status: number;
+  message: string;
+  data: UserInfo;
 }
 
 export interface UserInfo {
-	userId: number;
-	username: string;
-	nickname: string;
-	profileImg: string;
-	tierName: string;
-	tierId: number;
-	rankPercentile: number;
-	rank: number;
-	rankChange: number;
-	isOwner: boolean;
+  userId: number;
+  username: string;
+  nickname: string;
+  profileImg: string;
+  tierName: string;
+  tierId: number;
+  rankPercentile: number;
+  rank: number;
+  rankChange: number;
+  isOwner: boolean;
 }
 export interface SingleMatchData {
-	roomId: number;
-	leaderId: number;
-	users?: User[];
-	remainingUsers?: User[];
+  gameId?: number;
+  roomId: number;
+  leaderId: number;
+  users?: User[];
+  remainingUsers?: User[];
 }
 
 export interface SingleEnterApiResponse {
-	type?: string;
-	data: SingleMatchData;
-	message?: string;
-	status?: string;
+  type?: string;
+  data: SingleMatchData;
+  message?: string;
+  status?: string;
 }
 
 export interface RankingResponse {
-	userId?: number;
-	rank: number;
-	nickname: string;
-	mainLang: string;
-	rankPoint: number;
-	tierId: number;
-	rankChange: number;
-	averagePassRate: number;
+  userId?: number;
+  rank: number;
+  nickname: string;
+  mainLang: string;
+  rankPoint: number;
+  tierId: number;
+  rankChange: number;
+  averagePassRate: number;
 }
 export interface UserGameRecordResponse {
-	status: number;
-	message: string;
-	data: {
-		games: UserGameRecord[];
-		last: boolean;
-		totalPages: number;
-		currentPage: number;
-		totalElements: number;
-	};
+  status: number;
+  message: string;
+  data: {
+    games: UserGameRecord[];
+    last: boolean;
+    totalPages: number;
+    currentPage: number;
+    totalElements: number;
+  };
 }
 
 export interface UserGameRecord {
-	problem: Problem;
-	gameType: string;
-	startedAt: string;
-	totalHeadCount: number;
-	items: Item[];
-	myTeam: TeamInfo;
-	opponents: TeamInfo[];
+  problem: Problem;
+  gameType: string;
+  startedAt: string;
+  totalHeadCount: number;
+  items: Item[];
+  myTeam: TeamInfo;
+  opponents: TeamInfo[];
 }
 
 export interface Item {
-	itemId: number;
-	itemName: string;
+  itemId: number;
+  itemName: string;
 }
 
 export interface TeamInfo {
-	gameResult: string;
-	lang: string;
-	totalHeadCount: number;
-	executeTime: number | null;
-	executeMemory: number | null;
-	bonusPoint: number | null;
-	duration: string | null;
-	code: string | null;
-	members: MemberInfo[];
+  gameResult: string;
+  lang: string;
+  totalHeadCount: number;
+  executeTime: number | null;
+  executeMemory: number | null;
+  bonusPoint: number | null;
+  duration: string | null;
+  code: string | null;
+  members: MemberInfo[];
 }
 
 export interface MemberInfo {
-	userId: number;
-	nickname: string;
-	profileImage: string;
-	rank: number;
-	tierId: number;
+  userId: number;
+  nickname: string;
+  profileImage: string;
+  rank: number;
+  tierId: number;
 }
 
 export interface CodeInfo {
-	executeMemory: number;
-	executeTime: number;
-	code: string;
+  executeMemory: number;
+  executeTime: number;
+  code: string;
 }
