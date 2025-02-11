509b1340c8120ab06a6128f70b4c5ed7e2abffec
aodlfendb <heehyuon@gmail.com>
2025-02-11 22:34:15 +0900

    ♻️ [FE] refactor: 타입 삭제, 프리티어

--- altteul-fe/src/contexts/UserSearchContext.tsx
+++ altteul-fe/src/contexts/UserSearchContext.tsx
@@ -1,72 +1,61 @@
-import { extendedMockUsers } from "mocks/searchUserData";
-import React, { createContext, useState, ReactNode, useContext } from "react";
-
-// User 타입 정의
-type User = {
-  friendId: number;
-  nickname: string;
-  profileImg: string;
-  isOnline: boolean;
-};
+// import { extendedMockUsers } from "mocks/searchUserData";
+import React, { createContext, useState, ReactNode, useContext } from 'react';
+import { Friend, mockFriends } from 'mocks/friendData';
 
 type UserSearchContextType = {
   searchQuery: string;
-  searchResults: User[];
+  searchResults: Friend[];
   handleSearch: (query: string) => void;
-  resetSearch: () => void // 검색 초기화
+  resetSearch: () => void; // 검색 초기화
 };
 
-
 // Context 생성
-const UserSearchContext = createContext<UserSearchContextType | null>(null
-);
+const UserSearchContext = createContext<UserSearchContextType | null>(null);
 
 // Custom Hook
 export const useUserSearch = () => {
   const context = useContext(UserSearchContext);
-  if (context===null) {
-    throw new Error("useUserSearch must be used within a UserSearchProvider");
+  if (context === null) {
+    throw new Error('useUserSearch must be used within a UserSearchProvider');
   }
   return context;
 };
 
 // Provider 컴포넌트
 export const UserSearchProvider = ({ children }: { children: ReactNode }) => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<Friend[]>([]);
 
   const handleSearch = (query: string) => {
     setSearchQuery(query);
 
-    if (query.trim() === "") {
+    if (query.trim() === '') {
       setSearchResults([]);
       return;
     }
 
     // mockUsers 데이터로 검색
-    const filteredUsers = extendedMockUsers.filter((user) =>
+    const filteredUsers = mockFriends.filter(user =>
       user.nickname.toLowerCase().includes(query.toLowerCase())
     );
     setSearchResults(filteredUsers);
   };
 
-
   // 검색 초기화
-const resetSearch = () => {
-  setSearchQuery("")
-  setSearchResults([])
-  
-}
+  const resetSearch = () => {
+    setSearchQuery('');
+    setSearchResults([]);
+  };
   return (
-    <UserSearchContext.Provider value={{ 
-      searchQuery, 
-      searchResults, 
-      handleSearch,
-      resetSearch  // Provider에 resetSearch 함수 추가
-    }}>
+    <UserSearchContext.Provider
+      value={{
+        searchQuery,
+        searchResults,
+        handleSearch,
+        resetSearch, // Provider에 resetSearch 함수 추가
+      }}
+    >
       {children}
     </UserSearchContext.Provider>
   );
 };
-
-
