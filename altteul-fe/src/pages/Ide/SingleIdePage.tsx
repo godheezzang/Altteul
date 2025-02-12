bdc134692539cc43479d4534151ce7765b1ee9cc
박가희 <930_10@naver.com>
2025-02-12 11:04:08 +0900

    ✨ [FE] feat: 사이드 문제 기능 완료

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -7,6 +7,8 @@ import IdeFooter from '@components/Ide/IdeFooter';
 import ProblemInfo from '@components/Ide/ProblemInfo';
 import SideProblemModal from '@components/Ide/SideProblemModal';
 
+const MAX_REQUESTS = 5;
+
 const SingleIdePage = () => {
   const { gameId, roomId, users, problem } = useGameStore();
   const {
@@ -22,21 +24,26 @@ const SingleIdePage = () => {
   const [code, setCode] = useState('');
   const [language, setLanguage] = useState<'python' | 'java'>('python');
   const [showModal, setShowModal] = useState(false);
+  const [requestCount, setRequestCount] = useState(0)
 
   console.log(sideProblemResult);
 
   // ✅ 5분마다 자동으로 사이드 문제 요청
-  // useEffect(() => {
-  //   const interval = setInterval(
-  //     () => {
-  //       requestSideProblem();
-  //     },
-  //     // 5 * 60 * 1000
-  //     10 * 1000
-  //   ); // 5분
+  // TODO: 서버 버그 해결 시 주석 해제
+  useEffect(() => {
+    if (requestCount >= MAX_REQUESTS) return; // ✅ 5번 넘으면 실행 중단
 
-  //   return () => clearInterval(interval);
-  // }, [requestSideProblem]);
+    const interval = setInterval(() => {
+      if (requestCount < MAX_REQUESTS) {
+        requestSideProblem();
+        setRequestCount(prev => prev + 1); // ✅ 요청 횟수 증가
+      } else {
+        clearInterval(interval); // ✅ 5번 요청하면 멈춤
+      }
+    }, 5 * 60 * 1000); // 5분
+
+    return () => clearInterval(interval);
+  }, [requestCount, requestSideProblem]);
 
   // ✅ 사이드 문제가 도착하면 모달 띄우기
   useEffect(() => {
