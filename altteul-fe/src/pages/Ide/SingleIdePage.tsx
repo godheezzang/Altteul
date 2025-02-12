eeb78e1b626d3363b82802d065d91b9ba554753e
박가희 <930_10@naver.com>
2025-02-12 16:56:46 +0900

    ✅ [FE] update: 개인전 Ide 페이지 완료

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -20,6 +20,8 @@ const SingleIdePage = () => {
     requestSideProblem,
     submitSideProblemAnswer,
     submitCode,
+    completeUsers,
+    userProgress
   } = useGameWebSocket(gameId, roomId);
 
   const [code, setCode] = useState('');
@@ -31,20 +33,20 @@ const SingleIdePage = () => {
   console.log(sideProblemResult);
 
   // ✅ 5분마다 자동으로 사이드 문제 요청
-  // useEffect(() => {
-  //   if (requestCount >= MAX_REQUESTS) return; // ✅ 5번 넘으면 실행 중단
+  useEffect(() => {
+    if (requestCount >= MAX_REQUESTS) return; // ✅ 5번 넘으면 실행 중단
 
-  //   const interval = setInterval(() => {
-  //     if (requestCount < MAX_REQUESTS) {
-  //       requestSideProblem();
-  //       setRequestCount(prev => prev + 1); // ✅ 요청 횟수 증가
-  //     } else {
-  //       clearInterval(interval); // ✅ 5번 요청하면 멈춤
-  //     }
-  //   }, 5 * 60 * 1000); // 5분
+    const interval = setInterval(() => {
+      if (requestCount < MAX_REQUESTS) {
+        requestSideProblem();
+        setRequestCount(prev => prev + 1); // ✅ 요청 횟수 증가
+      } else {
+        clearInterval(interval); // ✅ 5번 요청하면 멈춤
+      }
+    }, 5 * 60 * 1000); // 5분
 
-  //   return () => clearInterval(interval);
-  // }, [requestCount, requestSideProblem]);
+    return () => clearInterval(interval);
+  }, [requestCount, requestSideProblem]);
 
   // ✅ 사이드 문제가 도착하면 모달 띄우기
   useEffect(() => {
@@ -62,10 +64,13 @@ const SingleIdePage = () => {
       <div className="max-w-[65rem] flex-[46rem] border-r border-gray-04">
         <CodeEditor code={code} setCode={setCode} language={language} setLanguage={setLanguage} />
         <Terminal output={output} />
+        <div className='text-center'>
         <IdeFooter code={code} language={language} setOutput={setOutput} />
+
+        </div>
       </div>
-      <div>
-        <GameUserList users={users}/>
+      <div className='grow max-w-[15rem]'>
+        <GameUserList users={users} completeUsers={completeUsers} userProgress={userProgress}/>
       </div>
       {/* ✅ 사이드 문제 모달 */}
       {showModal && sideProblem && (
