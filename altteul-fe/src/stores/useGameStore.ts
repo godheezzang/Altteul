bde4b17d8cd6daaec4ad7fbd968b739d560a0425
박가희 <930_10@naver.com>
2025-02-12 09:53:01 +0900

    ✏️ [FE] comments: 기본값 1 추후 수정 Todo 추가

--- altteul-fe/src/stores/useGameStore.ts
+++ altteul-fe/src/stores/useGameStore.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 
 const useGameStore = create<GameState>(set => ({
   // 사용자가 새로고침한 후에도 게임 정보를 유지하기 위해 로컬스토리지에 저장
+  // TODO: 현재는 테스트를 위해 기본값 1로 해둠. null로 수정할 것
   gameId: localStorage.getItem('gameId') ? Number(localStorage.getItem('gameId')) : 1,
   roomId: localStorage.getItem('roomId') ? Number(localStorage.getItem('roomId')) : 1,
   users: JSON.parse(localStorage.getItem('users') || '[]'),
