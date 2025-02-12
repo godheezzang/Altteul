1e1f21cdd56f45d6da84a7323c1a618232ac305a
aodlfendb <heehyuon@gmail.com>
2025-02-12 22:06:08 +0900

    💄 [FE] style: 중복확인 버튼 수정

--- altteul-fe/src/components/Common/Input.tsx
+++ altteul-fe/src/components/Common/Input.tsx
@@ -15,7 +15,8 @@ type InputProps = {
 
 const DEFAULT_INPUT_STYLE =
   'text-lg font-light text-primary-black bg-primary-white border-2 border-gray-02 rounded-lg px-4 py-2 w-full focus:ring-3 focus:ring-primary-orange focus:outline-none';
-const BUTTON_STYLE = 'ml-2 px-4 py-2 bg-primary-orange text-white rounded-lg cursor-pointer';
+const BUTTON_STYLE =
+  'ml-2 px-3 py-2 border border-primary-orange text-primary-orange bg-primary-white rounded-lg cursor-pointer hover:bg-gray-100 transition-colors border-2 '; // 호버배경 gray-100이 예뻐서 그냥 둠!
 
 const Input = ({
   type = 'text',
@@ -31,16 +32,18 @@ const Input = ({
 }: InputProps) => {
   return (
     <div className="flex items-center w-full">
-      <input
-        className={`${DEFAULT_INPUT_STYLE} w-[${width}] h-[${height}] ${className}`}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        name={name}
-      />
+      <div className="flex-grow" style={{ width: `calc(100% - ${buttonText ? '6rem' : '0rem'})` }}>
+        <input
+          className={`${DEFAULT_INPUT_STYLE} w-full h-[${height}] ${className}`}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          name={name}
+        />
+      </div>
       {buttonText && onButtonClick && (
-        <button onClick={onButtonClick} className={BUTTON_STYLE}>
+        <button type="button" onClick={onButtonClick} className={BUTTON_STYLE}>
           {buttonText}
         </button>
       )}
