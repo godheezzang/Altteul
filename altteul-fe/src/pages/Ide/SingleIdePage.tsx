0eb96519689e43d6ef92b031ff3fa8a7fb795221
박가희 <930_10@naver.com>
2025-02-12 13:57:33 +0900

    ✨ [FE] feat: 터미널 컴포넌트로 출력 결과 전송

--- altteul-fe/src/pages/Ide/SingleIdePage.tsx
+++ altteul-fe/src/pages/Ide/SingleIdePage.tsx
@@ -6,6 +6,7 @@ import Terminal from '@components/Ide/Terminal';
 import IdeFooter from '@components/Ide/IdeFooter';
 import ProblemInfo from '@components/Ide/ProblemInfo';
 import SideProblemModal from '@components/Ide/SideProblemModal';
+import GameUserList from '@components/Ide/GameUserList';
 
 const MAX_REQUESTS = 5;
 
@@ -25,25 +26,25 @@ const SingleIdePage = () => {
   const [language, setLanguage] = useState<'python' | 'java'>('python');
   const [showModal, setShowModal] = useState(false);
   const [requestCount, setRequestCount] = useState(0)
+  const [output, setOutput] = useState<string>('')
 
   console.log(sideProblemResult);
 
   // ✅ 5분마다 자동으로 사이드 문제 요청
-  // TODO: 서버 버그 해결 시 주석 해제
-  useEffect(() => {
-    if (requestCount >= MAX_REQUESTS) return; // ✅ 5번 넘으면 실행 중단
+  // useEffect(() => {
+  //   if (requestCount >= MAX_REQUESTS) return; // ✅ 5번 넘으면 실행 중단
 
-    const interval = setInterval(() => {
-      if (requestCount < MAX_REQUESTS) {
-        requestSideProblem();
-        setRequestCount(prev => prev + 1); // ✅ 요청 횟수 증가
-      } else {
-        clearInterval(interval); // ✅ 5번 요청하면 멈춤
-      }
-    }, 5 * 60 * 1000); // 5분
+  //   const interval = setInterval(() => {
+  //     if (requestCount < MAX_REQUESTS) {
+  //       requestSideProblem();
+  //       setRequestCount(prev => prev + 1); // ✅ 요청 횟수 증가
+  //     } else {
+  //       clearInterval(interval); // ✅ 5번 요청하면 멈춤
+  //     }
+  //   }, 5 * 60 * 1000); // 5분
 
-    return () => clearInterval(interval);
-  }, [requestCount, requestSideProblem]);
+  //   return () => clearInterval(interval);
+  // }, [requestCount, requestSideProblem]);
 
   // ✅ 사이드 문제가 도착하면 모달 띄우기
   useEffect(() => {
@@ -54,14 +55,17 @@ const SingleIdePage = () => {
 
   return (
     <div className="flex h-screen bg-primary-black border-t border-gray-04">
-      <div className="min-w-[30rem] border-r border-gray-04">
+      <div className="min-w-[23em] border-r border-gray-04">
         <ProblemInfo />
       </div>
 
       <div className="max-w-[65rem] flex-[46rem] border-r border-gray-04">
         <CodeEditor code={code} setCode={setCode} language={language} setLanguage={setLanguage} />
-        <Terminal output={codeResult ? JSON.stringify(codeResult, null, 2) : ''} />
-        <IdeFooter onExecute={() => submitCode(problem.problemId, language, code)} />
+        <Terminal output={output} />
+        <IdeFooter code={code} language={language} setOutput={setOutput} />
+      </div>
+      <div>
+        <GameUserList users={users}/>
       </div>
       {/* ✅ 사이드 문제 모달 */}
       {showModal && sideProblem && (
