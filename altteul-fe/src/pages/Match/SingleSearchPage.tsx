f5627361ecf373be20383fdb9ec5972aca2bb7b0
김덕진 <ghaehfdl7@naver.com>
2025-02-13 00:37:09 +0900

    ✨ [FE] feat: 개인전 매칭 탐색 완료

--- altteul-fe/src/pages/Match/SingleSearchPage.tsx
+++ altteul-fe/src/pages/Match/SingleSearchPage.tsx
@@ -1,116 +1,137 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { formatTime } from "@utils/formatTime";
-import UserProfile from "@components/Match/UserProfile";
-import Button from "@components/Common/Button/Button";
-import backgroundImage from "@assets/background/single_matching_bg.svg";
-import logo from "@assets/icon/Altteul.svg";
-import tmi from "@assets/tmi.json";
-import { useTimer } from "@hooks/useTimer";
-import { User } from "types/types";
-import useMatchWebSocket from "@hooks/useMatchWebSocket";
-import { useMatchStore } from "@stores/matchStore";
-import { useSocketStore } from "@stores/socketStore";
-import { singleOut, singleStart } from "@utils/Api/matchApi";
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { formatTime } from '@utils/formatTime';
+import UserProfile from '@components/Match/UserProfile';
+import Button from '@components/Common/Button/Button';
+import backgroundImage from '@assets/background/single_matching_bg.svg';
+import logo from '@assets/icon/Altteul.svg';
+import tmi from '@assets/tmi.json';
+import { useTimer } from '@hooks/useTimer';
+import { User } from 'types/types';
+import useMatchWebSocket from '@hooks/useMatchWebSocket';
+import { useMatchStore } from '@stores/matchStore';
+import { useSocketStore } from '@stores/socketStore';
+import { singleOut, singleStart } from '@utils/Api/matchApi';
 
 const SingleSearchPage = () => {
   const navigate = useNavigate();
-  const store = useMatchStore();
-  const socketStore = useSocketStore();
-  const [fact, setFact] = useState<string>("");
+  const matchStore = useMatchStore();
+  const socket = useSocketStore();
+  const [fact, setFact] = useState<string>('');
   const [facts] = useState<string[]>(tmi.facts);
-  const [waitUsers, setWaitUsers] = useState(store.matchData.users);
-  const [leaderId] = useState(store.matchData.leaderId);
-  const [headUser, setHeadUser] = useState<User>(
-    waitUsers.find((user) => user.userId === leaderId)
-  );
-  const roomId = store.matchData.roomId;
-  const currentUserId = Number(localStorage.getItem("userId"));
-  const isLeader = currentUserId === leaderId;
-  
+  const [leaderId] = useState(matchStore.matchData.leaderId);
+  //waitUsers: 방장을 포함하지 않은 대기 유저
+  const [waitUsers, setWaitUsers] = useState(matchStore.matchData.users.filter(user => user.userId !== leaderId));
+  const [headUser, setHeadUser] = useState<User>(matchStore.matchData.users.find(user => user.userId === leaderId));
+  const roomId = matchStore.matchData.roomId;
+  const currentUserId = Number(localStorage.getItem('userId'));
+
+  // TODO: 게임시작 버튼 렌더링을 위해 임시 조치
+  // const [isLeader, setIsLeader] = useState(currentUserId === leaderId);
+  const isLeader = true;
+
   // 타이머 완료 여부를 추적하는 상태 추가
   const [isTimeUp, setIsTimeUp] = useState(false);
-  
-  const { isConnected, error, c_waitUsers, c_leaderId } = useMatchWebSocket(roomId);
+  // 소켓에서 받아오는 부분 설정
+  const { isConnected, c_waitUsers, c_leaderId } = useMatchWebSocket(roomId);
 
-  // 타이머 설정
-  const { seconds } = useTimer({
-    initialSeconds: 10,
-    onComplete: () => {
-      setIsTimeUp(true);
-    },
-  });
-
-  // 타이머 완료 시 페이지 이동 처리
-  useEffect(() => {
-    if (isTimeUp) {
-      store.setMatchData({
-        data: {
-          roomId: roomId,
-          leaderId: leaderId,
-          users: [headUser, ...waitUsers],
-        }
-      });
-
-      //게임 시작 API 호출
-      const res = singleStart(roomId, leaderId, "time");
-      if (res === 200) {
-        navigate("/match/single/final");
-      }
-
-    }
-  }, [isTimeUp, roomId, leaderId, waitUsers, navigate]);
-
-  // Socket connection management
-  useEffect(() => {
-    socketStore.setKeepConnection(true);
-    return () => {
-      if (!socketStore.keepConnection) {
-        singleOut(currentUserId);
-        socketStore.resetConnection();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log("연결 상태확인: ", isConnected);
-  }, [isConnected]);
+  // (새로 고침도 언마운트 적용될 줄 알았을 때 작성한 코드), 일단 둬보고 삭제 예정
+  // 소켓 연결 관리 부분
+  // useEffect(() => {
+  //   //소켓 연결 유지는 페이지 넘어가기 전, 정상 페이지 이동에만 설정, 초기에는 false
+  //   //정상적인 페이지 이동이 아닌 경우 Defalut로 연결 끊기 위함
+  //   socket.setKeepConnection(false);
+  //   return () => {
+  //     // 소켓 연결 유지 선언을 하지 않았다면 연결 유지 초기화
+  //     if (!socket.keepConnection) {
+  //       console.log('!!연결 유지 선언이 없어서 소켓 연결을 초기화 합니다!!');
+  //       socket.resetConnection();
+  //       console.log("비정상적인 페이지 이탈 -> 방 나가기 로직 작동")
+  //       singleOut(currentUserId)
+  //       navigate('/match/select');
+  //     }
+  //   };
+  // },[]);
 
   // 유저 정보 업데이트
   useEffect(() => {
-    if (c_waitUsers && c_leaderId) {
-      console.log("유저정보 Update");
-      console.log("대기 유저 정보: ", c_waitUsers);
-      console.log("방장 ID: ", c_leaderId);
-      setHeadUser(c_waitUsers.find((user) => user.userId === c_leaderId));
-      setWaitUsers(c_waitUsers.filter((user) => user.userId !== c_leaderId));
-    }
-  }, [c_waitUsers, c_leaderId]);
+    if (isConnected && c_waitUsers && c_leaderId) {
+      //타이머 리셋
+      reset()
 
-  //타이머 전 게임 시작 호출
-  const userStart = () => {
-    if (confirm("아직 8명 안됐는데 시작할거임?")) {
-      if (waitUsers.length >= 2) {
-        store.setMatchData({
-          data: {
-            roomId: roomId,
-            leaderId: leaderId,
-            users: waitUsers,
-          }
-        });
-        navigate("/match/single/final");
-      } else {
-        alert("개인전이긴 한데... 너 혼자 게임 못함...");
+      // TODO: 게임시작 버튼 렌더링을 위해 임시 조치
+      // setIsLeader(currentUserId === c_leaderId);
+      setHeadUser(c_waitUsers.find(user => user.userId === c_leaderId));
+      setWaitUsers(c_waitUsers.filter(user => user.userId !== c_leaderId));
+
+      // 대기 유저가 8명이 되면 자동으로 게임 시작
+      if (c_waitUsers.length >= 8) {
+        handleStartButton();
       }
+    }
+  }, [isConnected, c_waitUsers, c_leaderId]);
+
+  // 타이머 설정
+  const { seconds, reset } = useTimer({
+    initialSeconds: 1,
+
+    // 타이머 완료 시 페이지 이동 처리
+    onComplete: () => {
+      console.log(waitUsers)
+      console.log(waitUsers.length === 0)
+      //1. 혼자만 있으면 시작 x
+      if (waitUsers.length === 0) {
+        alert('상대 유저가 입장하지 않아 종료합니다.')
+        userOut()
+        return;
+      }
+
+      //2. 방장 제외 1명 이상의 플레이어만 충족하면 시작
+      navigateFinalPage()
+    },
+  });
+
+  //게임 시작 버튼 클릭
+  const handleStartButton = async () => {
+    //혼자만 있을 때
+    if (waitUsers.length === 0) {
+      alert('상대 유저가 입장하지 않았습니다.');
+      return;
+    }
+
+    //8명이 됐는지 확인
+    if (waitUsers.length === 8 || confirm("바로 시작하시겠습니까?")) {
+      navigateFinalPage()
     }
   };
 
-  //유저(본인) 퇴장
+  //Final 페이지 이동 조건 충족시
+  const navigateFinalPage = async () => {
+    // Final 페이지로 넘어가기 전에 데이터 저장
+    matchStore.setMatchData({
+      data: {
+        roomId: roomId,
+        leaderId: leaderId,
+        users: [headUser, ...waitUsers],
+      },
+    });
+
+    //게임 시작 API 호출(For socket 응답 변환)
+    const res = await singleStart(roomId, leaderId);
+      if (res.status === 200) {
+        socket.setKeepConnection(true); // 연결 유지 상태로 변경
+        navigate('/match/single/final');
+      }else if (res.status === 400) {
+        console.log("게임 매칭 완료 중 에러 발생")
+      }
+  }
+
+  //유저 퇴장 로직
   const userOut = () => {
-    socketStore.resetConnection();
     singleOut(currentUserId);
-    navigate("/match/select");
+    socket.resetConnection();
+    navigate('/match/select');
   };
 
   // TMI: 첫 fact 생성 후 5초 간격으로 Rotation
@@ -121,16 +142,6 @@ const SingleSearchPage = () => {
     }, 5000);
     return () => clearInterval(factRotation);
   }, [facts]);
-
-  // WebSocket 상태 모니터링
-  useEffect(() => {
-    if (error) {
-      console.error("WebSocket 연결 오류:", error);
-      alert("연결에 문제가 발생했습니다. 다시 시도해주세요.");
-      socketStore.resetConnection();
-      navigate("/match/select");
-    }
-  }, [error, navigate]);
 
   return (
     <div
@@ -155,14 +166,14 @@ const SingleSearchPage = () => {
 
         {/* 방장: 리더아이디에 해당하는 유저 정보 넣어야 함*/}
         <UserProfile
-          nickname={headUser.nickname}
-          profileImg={headUser.profileImg}
-          tierId={headUser.tierId}
+          nickname={""}
+          profileImg={headUser? headUser.profileImg : null}
+          tierId={headUser? headUser.tierId : null}
           className="mb-4"
         />
 
         {/* 방장 이름 */}
-        <div className="text-white text-2xl mb-4">나는 방장</div>
+        <div className="text-white text-2xl mb-4">{headUser?.nickname}</div>
 
         {/* Status Message */}
         <div className="text-white text-xl mb-8 flex flex-col items-center">
@@ -181,7 +192,7 @@ const SingleSearchPage = () => {
           {isLeader && (
             <Button
               className="transition-all duration-300 hover:shadow-[0_0_15px_var(--primary-orange)]"
-              onClick={userStart}
+              onClick={handleStartButton}
             >
               게임 시작
             </Button>
@@ -197,7 +208,7 @@ const SingleSearchPage = () => {
         {/* 방장 제외 대기 유저 */}
         <div className="flex justify-center items-center gap-20">
           {waitUsers
-            .filter((user) => user.userId !== leaderId)
+            .filter(user => user.userId !== leaderId)
             .map((user: User) => (
               <UserProfile
                 key={user.userId}
