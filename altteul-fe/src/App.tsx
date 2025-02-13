b8f2b7cfcd318e3ac1ca63bc32d5288537381ad7
김덕진 <ghaehfdl7@naver.com>
2025-02-14 02:24:21 +0900

    ✨ [FE] feat: 로그인 시 최상위 컴포넌트에서 소켓 연결 유지

--- altteul-fe/src/App.tsx
+++ altteul-fe/src/App.tsx
@@ -1,12 +1,24 @@
 import GameGnb from '@components/Nav/GameGnb';
 import MainGnb from '@components/Nav/MainGnb';
 import { Outlet, useLocation } from 'react-router-dom';
-import ModalManager from '@components/common/ModalManager';
+import ModalManager from '@components/Common/ModalManager';
 import { UserSearchProvider } from 'contexts/UserSearchContext';
+import { useEffect } from 'react';
+import { useSocketStore } from '@stores/socketStore';
 
 const App = () => {
   const location = useLocation();
   const isGamePage = location.pathname.startsWith('/game');
+  const { connect, restoreSubscriptions } = useSocketStore();
+
+  //로그인 시 소켓 연결 유지
+  useEffect(() => {
+    const wasConnected = sessionStorage.getItem('wsConnected') === 'true';
+    const accessToken = sessionStorage.getItem('token');
+    if (wasConnected && !!accessToken) {
+      connect();
+    }
+  }, []);
 
   return (
     <UserSearchProvider>
