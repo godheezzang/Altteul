b09df85407ce7b6f8a48134451cb2b97f6b4931f
김덕진 <ghaehfdl7@naver.com>
2025-02-14 02:26:27 +0900

    🚧 [FE] cont: 전역 소켓에 맞춰 수정중

--- altteul-fe/src/pages/Match/SingleFinalPage.tsx
+++ altteul-fe/src/pages/Match/SingleFinalPage.tsx
@@ -1,10 +1,8 @@
 import useGameStore from '@stores/useGameStore';
 import { useNavigate, Link } from 'react-router-dom';
-import { formatTime } from '@utils/formatTime';
-import { useTimer } from '@hooks/useTimer';
 import backgroundImage from '@assets/background/single_matching_bg.svg';
 import logo from '@assets/icon/Altteul.svg';
-import { User } from 'types/types';
+import { Problem, TestCase, User } from 'types/types';
 import { useMatchStore } from '@stores/matchStore';
 import { useState, useEffect } from 'react';
 import UserProfile from '@components/Match/UserProfile';
@@ -16,7 +14,6 @@ const SingleFinalPage = () => {
   const navigate = useNavigate();
   const store = useMatchStore();
   const socket = useSocketStore()
-
   const roomId = store.matchData.roomId;
   const [leaderId] = useState(store.matchData.leaderId);
   // Store에 저장된 데이터로 초기 세팅
@@ -25,40 +22,40 @@ const SingleFinalPage = () => {
   // console.log("전달 받은 데이터::", store.matchData)
   const { setGameInfo, setUsers, setProblem, setTestcases } = useGameStore();
 
-  const { c_waitUsers, c_leaderId, count, isStart, gameData } = useMatchWebSocket(roomId);
+  // const { c_waitUsers, c_leaderId, count, isStart, gameData } = useMatchWebSocket(roomId);
+  const [isStart, setIsStart] = useState(false)
+  const [gameData, setGameData] = useState<{gameId:number, users:User[], problem:Problem, testcases:TestCase[]}>()
 
   const [seconds, setSeconds] = useState<number>(10)
 
+  //구독처리
   useEffect(() => {
-    if (c_waitUsers && c_leaderId) {
-      console.log('유저정보 Update');
-      console.log('대기 유저 정보: ', c_waitUsers);
-      console.log('방장 ID: ', c_leaderId);
-      setHeadUser(c_waitUsers.find(user => user.userId === c_leaderId));
-      setWaitUsers(c_waitUsers.filter(user => user.userId !== c_leaderId));
+    socket.subscribe(`/sub/single/room/${roomId}`, handleMessage)
+  }, [roomId])
+
+  //소켓 응답 처리
+  const handleMessage = (message) => {
+    const { type, data } = message;
+
+    if (type === 'ENTER' || type === 'LEAVE') {
+      setWaitUsers(data.users.filter(user => user.userId !== leaderId));
+      setHeadUser(data.users.find(user => user.userId === leaderId))
     }
-  }, [c_waitUsers, c_leaderId]);
 
-  //응답 데이터 수신에 따른 count 변화
-  useEffect(() => {
-    setSeconds((count))
-  }, [count])
+    if (type === 'COUNTING') {
+      setSeconds(data.time);
+    }
 
-  // 소켓 연결 관리 부분
-  useEffect(() => {
-    //소켓 연결 유지는 페이지 넘어가기 전에만 설정, 초기에는 false
-    //정상적인 페이지 이동이 아닌 경우 Defalut로 연결 끊기 위함
-    socket.setKeepConnection(false);
-    return () => {
-      // 소켓 연결 유지 선언을 하지 않았다면 연결 유지 초기화(소켓 끊음)
-      if (!socket.keepConnection) {
-        console.log('!!연결 유지 선언이 없어서 소켓 연결을 초기화 합니다!!');
-        singleOut(roomId)
-        socket.resetConnection();
-        navigate('/match/select');
-      }
-    };
-  },[])
+    if (type === 'GAME_START') {
+      setGameData({
+        gameId: data.gameId,
+        users: data.users,
+        problem: data.problem,
+        testcases: data.testcases
+      })
+      setIsStart(true);
+    }
+  }
 
   // GAME_START 수신 시 페이지 이동 처리
   useEffect(() => {
@@ -68,7 +65,6 @@ const SingleFinalPage = () => {
       setUsers(gameData.users);
       setProblem(gameData.problem);
       setTestcases(gameData.testcases);
-      socket.setKeepConnection(true); //정상 이동, 소켓 연결 유지
 
       setTimeout(() => {
         console.log('IDE 페이지 이동');
