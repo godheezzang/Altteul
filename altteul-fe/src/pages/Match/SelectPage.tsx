e6873b2dd9f13369fc0464bc47e551ae78ee9ee1
김덕진 <ghaehfdl7@naver.com>
2025-02-15 03:13:45 +0900

    ♻️ [FE] refactor: 방 입장 로직 통합

--- altteul-fe/src/pages/Match/SelectPage.tsx
+++ altteul-fe/src/pages/Match/SelectPage.tsx
@@ -1,8 +1,7 @@
 import backgroundImage from "@assets/background/matching_select_bg.svg";
 import userIcon from "@assets/icon/User.svg";
 import peopleIcon from "@assets/icon/People.svg";
-import logo from "@assets/icon/Altteul.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "@styles/Base/colors.css";
 import { singleEnter, teamEnter } from "@utils/Api/matchApi";
 import { useMatchStore } from "@stores/matchStore";
@@ -10,43 +9,23 @@ import { useMatchStore } from "@stores/matchStore";
 const SelectPage = () => {
   const navigate = useNavigate();
   const userId = Number(localStorage.getItem("userId"));
-  const { setMatchData, setLoading } = useMatchStore();
+  const { setLoading } = useMatchStore();
 
-  //개인전 입장 로직
-  const singleNavigate = async () => {
+  const NextPage = async (type:string) => {
     setLoading(true); //개인전 버튼 눌렀을 때, finally로 가기 전까지 응답이 오래걸리면 스피너 효과 적용
     try {
-      //API 통신으로 방 입장 시의 응답값을 받아옴
-      const data = await singleEnter(userId);
+      //API로 방 입장 시의 응답값을 받아온 뒤 세션에 저장
+      sessionStorage.setItem("matchData", await singleEnter(userId))
 
-      //가져온 데이터를 Store에 저장
-      //TODO: sessionStorage 관리로 변경 필요
-      setMatchData(data);
-
-      //개인전 매칭 페이지 전환
-      navigate("/match/single/search")
-    }catch(error){
-      console.log(error)
-      // 에러페이지로 전환
-      // navigate()
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  //팀전 입장 로직
-  const teamNavigate = async () => {
-    setLoading(true); //개인전 버튼 눌렀을 때, finally로 가기 전까지 응답이 오래걸리면 스피너 효과 적용
-    try {
-      //API 통신으로 방 입장 시의 응답값을 받아옴
-      const data = await teamEnter(userId);
-
-      //가져온 데이터를 Store에 저장
-      //TODO: sessionStorage 관리로 변경 필요
-      setMatchData(data);
-
-      //개인전 매칭 페이지 전환
-      navigate(`/match/team/composition/${data.data.roomId}`)
+      //개인전 입장
+      if(type === 'single') {
+        navigate("/match/single/search")
+      }
+      
+      //팀전 입장
+      if(type === 'team') {
+        navigate(`/match/team/composition`)
+      }
     }catch(error){
       console.log(error)
       // 에러페이지로 전환
@@ -68,14 +47,14 @@ const SelectPage = () => {
       <div className="relative flex gap-x-[40vh] items-center">
         {/* 개인전 버튼 링크 */}
         <button className="flex flex-col items-center w-[150px] h-[150px] rounded-full transition-all duration-500 hover:shadow-[0_0_30px_var(--primary-orange)]"
-        onClick={() => singleNavigate()}>
+        onClick={() => NextPage('single')}>
           <img src={userIcon} alt="개인전" className="w-full h-full" />
           <span className="mt-4 text-white text-[32px] font-bold">개인전</span>
         </button>
 
         {/* 팀전 버튼 링크 */}
         <button className="flex flex-col items-center w-[150px] h-[150px] rounded-full transition-all duration-500 hover:shadow-[0_0_30px_var(--primary-orange)]"
-        onClick={() => teamNavigate()}>
+        onClick={() => NextPage('team')}>
           <img src={peopleIcon} alt="팀전" className="w-full h-full" />
           <span className="mt-4 text-white text-[32px] font-bold">팀전</span>
         </button>
