60e19a3f5d27ba7eff7b4ae0bf9e7a0808937cd5
박가희 <930_10@naver.com>
2025-02-14 17:50:46 +0900

    ✅ [FE] update: gameData 기본값 설정

--- altteul-fe/src/stores/useGameStore.ts
+++ altteul-fe/src/stores/useGameStore.ts
@@ -3,13 +3,10 @@ import { create } from 'zustand';
 
 const useGameStore = create<GameState>(set => ({
   // 사용자가 새로고침한 후에도 게임 정보를 유지하기 위해 로컬스토리지에 저장
-  // TODO: 현재는 테스트를 위해 기본값 1로 해둠. null로 수정할 것
-  gameId: localStorage.getItem('gameId') ? Number(localStorage.getItem('gameId')) : 1,
-  roomId: localStorage.getItem('roomId') ? Number(localStorage.getItem('roomId')) : 1,
-  users: JSON.parse(localStorage.getItem('users') || `[{"roomId":1, "userId":2,"nickname":"닉넴2","profileImg":"", "tierId":"1"},
-			  {"roomId":2, "userId":3,"nickname":"닉넴3","profileImg":"", "tierId":"2"},
-			  {"roomId":3, "userId":4,"nickname":"닉넴4","profileImg":"", "tierId":"3"}]`),
-  problem: JSON.parse(localStorage.getItem('problem') || '{"problemId": 1}'),
+  gameId: localStorage.getItem('gameId') ? Number(localStorage.getItem('gameId')) : null,
+  roomId: localStorage.getItem('roomId') ? Number(localStorage.getItem('roomId')) : null,
+  users: JSON.parse(localStorage.getItem('users') || `[]`),
+  problem: JSON.parse(localStorage.getItem('problem') || '{}'),
   testcases: JSON.parse(localStorage.getItem('testcases') || '[]'),
 
   setGameInfo: (gameId: number, roomId: number) => {
