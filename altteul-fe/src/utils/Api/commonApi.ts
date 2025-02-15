0bea520e5cf558209e9012daa4ba12a85480dfe9
김덕진 <ghaehfdl7@naver.com>
2025-02-16 03:33:57 +0900

    🐛 [FE] fix: 주소 주석 삭제

--- altteul-fe/src/utils/Api/commonApi.ts
+++ altteul-fe/src/utils/Api/commonApi.ts
@@ -1,7 +1,6 @@
 // src/config/api.ts
 import axios from 'axios';
 
-// const baseURL = /api || 'http://localhost:8080/api';
 const BASE_URL = import.meta.env.MODE === 'production'
   ? import.meta.env.VITE_API_URL_PROD
   : import.meta.env.VITE_API_URL_DEV;
