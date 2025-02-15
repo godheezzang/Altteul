bd06f9facd4cda3c0b9f80687ebd736a0b28d50a
박가희 <930_10@naver.com>
2025-02-15 15:03:34 +0900

    🐛 [FE] fix: 메인페이지 JSON.parse 오류 수정

--- altteul-fe/src/App.tsx
+++ altteul-fe/src/App.tsx
@@ -6,7 +6,6 @@ import { UserSearchProvider } from 'contexts/UserSearchContext';
 import { useEffect } from 'react';
 import { useSocketStore } from '@stores/socketStore';
 import { inviteResponse } from '@utils/Api/matchApi';
-import { useMatchStore } from '@stores/matchStore';
 import socketResponseMessage from 'types/socketResponseMessage';
 
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
   const isGamePage = location.pathname.startsWith('/game');
   const socket = useSocketStore();
   const navigate = useNavigate();
-  const { setMatchData } = useMatchStore();
 
   const checkAuthStatus = () => {
     const accessToken = sessionStorage.getItem('token');
@@ -24,37 +22,43 @@ const App = () => {
   //로그인 시 소켓 연결 유지
   useEffect(() => {
     const wasConnected = sessionStorage.getItem('wsConnected') === 'true';
-    if (wasConnected && checkAuthStatus) {
-      try{
-        socket.connect() //로그인 성공시 소켓 연결
-      }catch(error){
-        console.log("소켓 연결 error 발생")
+    if (wasConnected && checkAuthStatus()) {
+      try {
+        socket.connect(); //로그인 성공시 소켓 연결
+      } catch (error) {
+        console.error('소켓 연결 error:', error);
       }
     }
-  }, []);
+  }, [socket]);
 
   // 로그인 & 소켓 연결 성공 시 친구관련 구독 신청
   //TODO: App.tsx기 때문에 페이지에 따른 구독신청과 구독취소 관리 필요함(안하면 문제 풀다가 초대 요청 받을 수 있음)
   useEffect(() => {
-    console.log("소켓 연결상태: ", socket.connected)
-    const userId = sessionStorage.getItem('userId')
-    if(socket.connected) {
-      socket.subscribe(`/sub/invite/${userId}`, handleMessage)  //친구 초대 구독
+    console.log('소켓 연결상태: ', socket.connected);
+    const userId = sessionStorage.getItem('userId');
+    if (socket.connected && userId) {
+      socket.subscribe(`/sub/invite/${userId}`, handleMessage); //친구 초대 구독
     }
-  }, [socket.connected])
+  }, [socket.connected]);
 
-  const handleMessage = async (message : socketResponseMessage) => {
-    const {type, data} = message
-    if (type === "INVITE_REQUEST_RECEIVED") {
+  const handleMessage = async (message: socketResponseMessage) => {
+    const { type, data } = message;
+    if (type === 'INVITE_REQUEST_RECEIVED') {
       //TODO: confirm 말고 다른 방식의 요청 수락/거절 형식 필요
-      if(confirm(`${data.nickname}님이 팀전에 초대하셨습니다.`)) {
-        //TODO: 응답(res.status)에 따른 처리 필요
-        const res = await inviteResponse(data.nickname, data.roomId, true)  //친구 초대 수락 api
-        sessionStorage.setItem("matchData", JSON.stringify(data)) //구성창에서 쓰일 데이터 설정
-        navigate(`/match/team/composition`)  //팀전 대기방으로 이동
+      if (confirm(`${data.nickname || '알 수 없음'}님이 팀전에 초대하셨습니다.`)) {
+        try {
+          //TODO: 응답(res.status)에 따른 처리 필요
+          const res = await inviteResponse(data?.nickname, data?.roomId, true); //친구 초대 수락 api
+          if (data) {
+            sessionStorage.setItem('matchData', JSON.stringify(data)); //구성창에서 쓰일 데이터 설정
+          }
+          navigate(`/match/team/composition`); //팀전 대기방으로 이동
+        } catch (error) {
+          console.error('초대 수락 중 오류 발생:', error);
+        }
       }
     }
-  }
+  };
 
   const hideNavigation = [
     '/match/team/composition',
