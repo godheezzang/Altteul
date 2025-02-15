adb5cbf74d8963656cedb049a6a2250823b95671
김덕진 <ghaehfdl7@naver.com>
2025-02-15 17:49:31 +0900

    ♻️ [FE] refactor: 개인전 팀전 입장 API 로직 통합

--- altteul-fe/src/utils/api/matchApi.ts
+++ altteul-fe/src/utils/api/matchApi.ts
@@ -2,18 +2,16 @@
 import { sigleApi, teamApi } from "@utils/Api/commonApi";
 import axios from 'axios';
 
-//싱글 매칭 입장시 사용 api
-export const singleEnter = async (userId: number) => {
+//매칭 입장시 사용 api
+export const matchRoomEnter = async (type:string) => {
   try {
-    const res = await sigleApi.post("enter")
+    const res = type ==='single' ? await sigleApi.post("enter") : await teamApi.post('enter')
     if (res.data.status === 200) {
       return res.data
     }
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
       console.log("개인전 입장 error: ", (error.response.data as { message: string }).message)
-      console.log("방 나가기 로직을 실행합니다, 다시 입장해주세요")
-      singleOut(Number(userId))
     }
   }
 }
@@ -31,20 +29,6 @@ export const singleStart = async (roomId: number) => {
 }
 
 //팀전 입장 api
-export const teamEnter = async (userId: number) => {
-  try {
-    const res = await teamApi.post("enter", { "userId": userId })
-    if (res.data.status === 200) {
-      return res.data
-    }
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      console.log("팀전 입장 error: ", (error.response.data as { message: string }).message)
-      console.log("방 나가기 로직을 실행합니다, 다시 입장해주세요")
-      teamOut(Number(userId))
-    }
-  }
-}
 
 //팀전 퇴장 api
 export const teamOut = async (userId: number) => {
