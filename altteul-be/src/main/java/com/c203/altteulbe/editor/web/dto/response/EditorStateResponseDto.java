5efbe199d981cd71c9db4e228281129e22d5fe96
김성찬 <hjkim2661@gmail.com>
2025-02-17 08:59:46 +0900

    ♻️ [BE] refactor: 공동 편집 부분 수정

--- altteul-be/src/main/java/com/c203/altteulbe/editor/web/dto/response/EditorStateResponseDto.java
+++ altteul-be/src/main/java/com/c203/altteulbe/editor/web/dto/response/EditorStateResponseDto.java
@@ -1,19 +1,20 @@
 package com.c203.altteulbe.editor.web.dto.response;
 
+import java.util.Map;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class EditorResponseDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class EditorStateResponseDto {
+	private String content;
 
-	private Long roomId;
+	private Map<String, Object> awareness;
 
 	private Long userId;
-
-	private String content;
 }
