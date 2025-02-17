a89e9a901473f912d44d3eced6acc670e7bae4f5
김덕진 <ghaehfdl7@naver.com>
2025-02-17 16:05:32 +0900

    ✨ [FE] feat: 채팅 관련 솟켓 응답 추가

--- altteul-fe/src/types/socketResponseMessage.ts
+++ altteul-fe/src/types/socketResponseMessage.ts
@@ -1,10 +1,21 @@
-import { FriendRequest, MatchData, Problem, TestCase, User } from "./types";
+import { FriendRequest, MatchData, Problem, TestCase, User } from './types';
 
 interface socketResponseMessage {
-  type: 'ENTER' | 'LEAVE' | 'COUNTING' | 'GAME_START' | "COUNTING_CANCEL"
-      | 'MATCHING' | 'MATCH_CANCEL_SUCCESS' | 'MATCHED' | 'COUNTING_READY'
-      | 'INVITE_REQUEST_RECEIVED'
-      | 'SEND_REQUEST';
+  type:
+    | 'ENTER'
+    | 'LEAVE'
+    | 'COUNTING'
+    | 'GAME_START'
+    | 'COUNTING_CANCEL'
+    | 'MATCHING'
+    | 'MATCH_CANCEL_SUCCESS'
+    | 'MATCHED'
+    | 'COUNTING_READY'
+    | 'INVITE_REQUEST_RECEIVED'
+    | 'INVITE_ACCEPTED'
+    | 'INVITE_REJECTED'
+    | 'SEND_REQUEST'
+    | '새 메시지';
   data: {
     //유저 입퇴장('ENTER/LEAVE') message
     leaderId?: number;
@@ -16,8 +27,8 @@ interface socketResponseMessage {
 
     //게임 시작(GAME_START) message
     gameId?: number;
-    problem?: Problem
-    testcases?: TestCase[]
+    problem?: Problem;
+    testcases?: TestCase[];
 
     //인원 미달(COUNTING_CANCEL) message
     note?: string;
@@ -25,19 +36,25 @@ interface socketResponseMessage {
     //게임 초대(INVITE_REQUEST_RECEIVED) message
     nickname?: string;
     roomId?: number;
-    
+
     //매칭 성사(MATCHED) message
-    matchId: string
+    matchId?: string;
 
     //팀전 게임 데이터(COUNTING_READY) message
-    team1: MatchData
-    team2: MatchData
+    team1?: MatchData;
+    team2?: MatchData;
 
     //친구 신청(SEND_REQUEST) message
-    friendRequests: FriendRequest
+    friendRequests?: FriendRequest;
 
+    //채팅(새 메시지) message
+    chatMessageId?: number;
+    senderId?: number;
+    senderNickname?: string;
+    messageContent?: string;
+    checked?: false;
+    createdAt?: string;
   };
-};
+}
 
-
-export default socketResponseMessage
+export default socketResponseMessage;
