678ddd400424348a2197e88e650d733b187ad80f
박가희 <930_10@naver.com>
2025-02-17 17:28:43 +0900

    💄 [FE] style: 개인/팀전별 터미널 좌우 마진 추가

--- altteul-fe/src/components/Ide/Terminal.tsx
+++ altteul-fe/src/components/Ide/Terminal.tsx
@@ -1,10 +1,13 @@
 interface TerminalProps {
   output: string;
+  isTeam: boolean;
 }
 
-const Terminal = ({ output }: TerminalProps) => {
+const Terminal = ({ output, isTeam }: TerminalProps) => {
   return (
-    <div className="h-[25vh] bg-gray-04 text-gray-02 p-3 overflow-auto ml-[4rem] mr-[1rem]">
+    <div
+      className={`h-[25vh] bg-gray-04 text-gray-02 p-3 overflow-auto ${isTeam && 'ml-[4rem] mr-[1rem]'}`}
+    >
       <pre className="whitespace-pre-wrap text-sm text-gray-01">{output}</pre>
     </div>
   );
