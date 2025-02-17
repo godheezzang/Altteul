7303e43381143f71081ce766f603a821a0ff95eb
jinwooseok <jinus7949@naver.com>
2025-02-18 04:31:03 +0900

    🔧 [BE] conf: yjs 의존성 주입

--- altteul-fe/vite.config.ts
+++ altteul-fe/vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
     allowedHosts: [
       'i12c203.p.ssafy.io',
       'localhost',
-      'localhost:5173'
+      'localhost:5173',
+      'ec2-18-212-95-231.compute-1.amazonaws.com'
     ]
   }
 });
